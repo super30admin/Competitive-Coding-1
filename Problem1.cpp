39014156c05a90e191735d56fb1f463e810d9754
GAURAV JOSHI <gsj38@msstate.edu>
2019-07-08 03:12:17 -0500

    Updated problem1 solution to work for edge cases

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+/*
+Works on all edge cases (when array doesn't start from 1)
+
+Complexity: O(logn)
+*/
+
+class Solution {
+    public:
+        int Search(vector<int>arr)
+        {
+            int start = 0;
+            int end = arr.size() - 1;
+            int middle;
+            
+            while(start <= end)
+            {   
+                middle = start + (end - start)/2;
+                if(arr[middle] == middle + arr[0])
+                {
+                    if(arr[middle + 1] == middle + arr[1])
+                        start = middle+1;
+                    else
+                        return middle + arr[1]; 
+                }
+        
+                else 
+                {
+                    if(arr[middle - 1] == middle + arr[0])
+                        end = middle -1;
+                    else
+                        return middle + arr[0];
+                }
+            }
+            return -1;
+        }
+};
+
+int main() {
+	Solution s;
+	vector<int>vec{21,22,24,25,26};
+	cout<<s.Search(vec);
+	return 0;
+}
--- Problem2.cpp
+++ Problem2.cpp
@@ -1,1 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+// StackNode 
+class StackNode { 
+    public:
+        int data; 
+        int priority; 
+        StackNode* next; 
+  
+}; 
+  
+// Function to Create A New Node 
+StackNode* newNode(int data, int priority) 
+{ 
+    StackNode* node = new StackNode();
+    node->data = data;
+    node->next = NULL;
+    node->priority = priority;
+    return node; 
+} 
+  
+// Return the value at head 
+int peek(StackNode** head) 
+{ 
+    return (*head)->data; 
+} 
+  
+// Removes the element with the 
+// highest priority form the list 
+int pop(StackNode** head) 
+{   
+    StackNode* temp = *head; 
+    (*head) = (*head)->next; 
+    int popped = temp->data;
+    delete temp;
+    return popped; 
+} 
+  
+// Function to push according to priority 
+void push(StackNode** head, int data, int priority) 
+{ 
+    StackNode* start = (*head); 
+  
+    // Create new Node 
+    StackNode* temp = newNode(data, priority); 
+  
+    // Special Case: The head of list has lesser 
+    // priority than new node. So insert new 
+    // node before head node and change head node. 
+    if ((*head)->priority > priority) { 
+  
+        // Insert New Node before head 
+        temp->next = *head; 
+        (*head) = temp; 
+    } 
+    else { 
+  
+        // Traverse the list and find a 
+        // position to insert new node 
+        while (start->next != NULL && 
+               start->next->priority < priority) { 
+            start = start->next; 
+        } 
+  
+        // Either at the ends of the list 
+        // or at required position 
+        temp->next = start->next; 
+        start->next = temp; 
+    } 
+} 
+  
+// Function to check is list is empty 
+int isEmpty(StackNode** head) 
+{ 
+    return (*head) == NULL; 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    // Create a Priority Queue 
+    // 7->4->5->6 
+    StackNode* pq = newNode(4, 1); 
+    push(&pq, 5, 2); 
+    push(&pq, 6, 3); 
+    push(&pq, 7, 0); 
+  
+    while (!isEmpty(&pq)) { 
+        printf("%d ", peek(&pq)); 
+        pop(&pq); 
+    } 
+  
+    return 0; 
+} 
