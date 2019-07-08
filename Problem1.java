a40577c11b7c1f394dc87ca8505a084435ae6ca1
Sarang <saranggrover4488@gmail.com>
2019-07-07 21:18:25 -0400

    brute force solution. working on binary search

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,20 @@
 
+public class FindRemaining {
+	
+	public static int find(int arr[]) {
+		int length = arr.length;
+		int total = (length+1)*(length+2)/2;
+		for(int i=0;i<length;i++) {
+			total-=arr[i];
+		}
+		return total;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 int[]arr= {1,2,3,5,6,7,8};
+	 System.out.println(find(arr));
+
+	}
+
+}
