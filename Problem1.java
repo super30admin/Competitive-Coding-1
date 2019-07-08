2fce54a0117159d7f78284c38da2e05fd2765fd3
Sehaj grover <sehaj131@gmail.com>
2019-07-08 07:09:46 +0530

    please review

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,32 @@
 
+class Solution {
+	
+	public static int findPeak(int[] arr)
+	{
+		int mid,start=0,end=arr.length-1;
+		boolean flag = true;
+		while(start<=end)
+		{
+			mid=start+(end-start)/2;
+			//System.out.print(mid);
+			if(!check(arr,mid)) {return arr[mid]+1;}
+			if(arr[mid]!=mid+1) {end=mid-1;}
+			else {start=mid+1;}
+		}
+		return -1;
+
+	}
+	public static boolean check(int[] arr, int pos)
+	{
+		if((pos==arr.length-1 || arr[pos+1]-arr[pos]==1) && (arr[pos]-arr[pos-1]==1	|| pos==0))return true;
+		return false;
+	}
+    public static void main(String args[]) 
+    { 
+    	int[] arr = {1,3,4,5,6};
+    	int peak=findPeak(arr);
+    	System.out.println(peak);
+    	
+        
+    } 
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,60 @@
 
+class PriorityQueue
+{
+	
+	class QueueNode
+	{
+		int data,priority;
+		QueueNode next;
+		
+		QueueNode(int data,int priority){this.data = data;this.priority=priority;}
+	}
+	QueueNode root = null;
+	
+	public boolean isEmpty(){return root==null;} 
+	  
+    public void push(int data,int priority) 
+    { 
+    	QueueNode node= root;
+    	QueueNode temp= new QueueNode(data, priority);
+    	if(this.isEmpty() || root.priority>temp.priority)
+    	{
+    		temp.next=root;
+    		root=temp;
+    	}
+    	else 
+    	{
+    		while(node.next!=null && node.next.priority<priority)
+    		{
+    			node=node.next;
+    		}
+    		temp.next=node.next;
+    		node.next=temp;
+    		
+    		
+    	}
+    	   
+    } 
+  
+    public int pop() 
+    { 	
+    	if(this.isEmpty()) {System.out.println("Stack Underflow"); return 0;}
+    	int value=root.data;
+    	root=root.next;
+    	return value;
+    } 
+  
+    public int peek()
+    {	if(this.isEmpty()){System.out.println("Stack is empty"); return -1;}
+    	return root.data;} 
+    
+    public static void main(String args[])
+    {
+    	PriorityQueue queue= new PriorityQueue() ;
+    	queue.push(1, 1);
+    	queue.push(2, 0);
+    	queue.push(3, 3);
+    	while(!queue.isEmpty()) { System.out.println(queue.pop());}
+    	
+    }
+}
