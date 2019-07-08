756122849fe74845554713d155fec46dfeffebb8
shazmaan <shazmaan@buffalo.edu>
2019-07-08 05:34:17 +0400

    Problem1 & 2

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,1 +1,32 @@
+//
+// Created by shazmaan on 7/8/2019.
+//
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    int problem(vector<int>& nums){
+        int low = 0; int high = nums.size()-1; int mid;
+        while(low<=high){
+            mid = low + (high-low)/2;
+            if(nums[mid]!=(mid+1) && nums[mid-1]==mid){
+                return mid+1;
+            }else if(nums[mid]== mid+1){
+                low = mid +1;
+            }else{
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
+};
+
+int main(){
+    Solution s;
+    return 0;
+}
--- Problem2.cpp
+++ Problem2.cpp
@@ -1,1 +1,75 @@
+//
+// Created by shazmaan on 7/8/2019.
+//
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class StackNode{
+public:
+    int data;
+    StackNode* next;
+    int index;
+};
+
+StackNode* newNode(int data)
+{
+    StackNode* stackNode = new StackNode();
+    stackNode->data = data;
+    stackNode->next = NULL;
+    stackNode->index = index;
+    return stackNode;
+}
+
+int isEmpty(StackNode* root)
+{
+    if(root==NULL){
+        return true;
+    }
+    return false;//Your code here
+}
+
+void push(StackNode** root, int data, int pos)
+{
+    StackNode* node = newNode(data);
+    StackNode* temp = (*root);
+    while(temp->index < pos  && temp->next != NULL){
+        (temp) = (temp)->next;
+    }
+    node->next = temp;
+    temp = node;
+    //Your code here
+}
+
+int pop(StackNode** root)
+{
+    if(isEmpty((*root))){
+        cout<<"Stack Underflow"<<endl;
+        return 0;
+    }
+    StackNode* temp = (*root);
+    (*root) = (*root)->next;
+    int ret = temp->data;
+    delete temp;
+    return ret;
+    //Your code here
+}
+
+int peek(StackNode* root)
+{
+    if(isEmpty(root)){
+//        cout<<"Stack Underflow"<<endl;
+        return 0;
+    }
+    return root->data;
+    //Your code here
+}
+};
+
+int main(){
+    StackNode s;
+    return 0;
+}
