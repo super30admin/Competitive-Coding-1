65c5848d8501b92f426169f51bdc5a3cf0f3b581
Sarang <saranggrover4488@gmail.com>
2019-07-07 23:05:41 -0400

    priority queue bug fix

--- Problem2.java
+++ Problem2.java
@@ -1,28 +1,33 @@
 
 public class PriorityQueue {
+	
 	Node head;
 	
 	public class Node{
 		int value;
 		int priority;
-	    Node next;
+		Node next;
 		
-		public Node(int value,int priority) {
-			this.value=value;this.priority=priority;
+		public Node(int value, int priority) {
+			this.value=value;
+			this.priority=priority;
 		}
 	}
+		
 	
-	public void push(int value, int priority) {
-		Node temp = new Node(value,priority);
+	
+	public void push(int value,int priority) {
+		Node temp= new Node(value,priority);
 		if(this.head==null) {
-			head = temp;
+			head=temp;
 		}else {
 			if(head.priority>priority) {
 				temp.next=head;
 				head=temp;
 			}else {
 				Node start = head;
-				while(start.next!=null && start.priority<priority) {
+				Node prev = null;
+				while(start.next!=null && start.next.priority<priority) {
 					start = start.next;
 				}
 				temp.next = start.next;
@@ -39,19 +44,26 @@ public class PriorityQueue {
 	
 	public int peek() {
 		return head.value;
+	
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-     PriorityQueue p = new PriorityQueue();
-     p.push(5, 2);
-     p.push(4, 3);
-     p.push(7, 0);
-     
-     System.out.println(p.peek());
-     p.pop();
-     System.out.println(p.peek());
+		 PriorityQueue p = new PriorityQueue();
+	     p.push(1,1);
+	     p.push(2, 2);
+	     p.push(4, 4);
+	     p.push(3, 3);
+	     
+	    
+
+	     System.out.println(p.peek());
+	     p.pop();
+	     System.out.println(p.peek());
+	     p.pop();
+	     System.out.println(p.peek());
+	    p.pop();
+	     System.out.println(p.peek());
 
 	}
 
