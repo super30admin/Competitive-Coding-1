0b087a67b14fe6d286574c5ddf115b93fdc3853f
Sarang <saranggrover4488@gmail.com>
2019-07-07 21:38:07 -0400

    implemented priority queue

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,58 @@
 
+public class PriorityQueue {
+	Node head;
+	
+	public class Node{
+		int value;
+		int priority;
+	    Node next;
+		
+		public Node(int value,int priority) {
+			this.value=value;this.priority=priority;
+		}
+	}
+	
+	public void push(int value, int priority) {
+		Node temp = new Node(value,priority);
+		if(this.head==null) {
+			head = temp;
+		}else {
+			if(head.priority>priority) {
+				temp.next=head;
+				head=temp;
+			}else {
+				Node start = head;
+				while(start.next!=null && start.priority<priority) {
+					start = start.next;
+				}
+				temp.next = start.next;
+				start.next=temp;
+			}
+		}
+	}
+	
+	public Node pop() {
+		Node temp = head;
+		head = head.next;
+		return temp;
+	}
+	
+	public int peek() {
+		return head.value;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+     PriorityQueue p = new PriorityQueue();
+     p.push(5, 2);
+     p.push(4, 3);
+     p.push(7, 0);
+     
+     System.out.println(p.peek());
+     p.pop();
+     System.out.println(p.peek());
+
+	}
+
+}
