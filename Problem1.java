effe3e359ccfcc0af3a5bc655b4738907845dbe2
Manogna Mujje <manogna.mujje@gmail.com>
2019-08-05 19:32:40 -0700

    Updated comments

--- Problem1.java
+++ Problem1.java
@@ -8,11 +8,16 @@ public class Problem1 {
 	private static int findMissingNum(int[] nums) {
 		int lo = 0, hi = nums.length-1;
 		if(nums[0] == 2) return 1;
-		// Loop terminating condition
+		//	Loop terminating condition
 		while(lo<hi) {
-			//Condition when solution is found
+			//	Condition when solution is found
+			// 	When the numbers are consecutive and their diff value is 1 and 2 respectively, middle element is the missing one
 			if(hi-lo==1) {
-				return nums[hi]-1;
+				if(diff(nums, hi) == 2 && diff(nums, lo) == 1) {
+					return nums[hi]-1;
+				} else {
+					return -1;
+				}
 			} else {
 				int mid = lo+(hi-lo)/2;
 				if(diff(nums, mid) == 1) {
@@ -26,7 +31,7 @@ public class Problem1 {
 		return -1;
 	}
 	
-	//Returns difference between index and its element
+	//	Returns the difference between index and its element
 	private static int diff(int[] nums, int i) {
 		return nums[i]-i;
 	}
