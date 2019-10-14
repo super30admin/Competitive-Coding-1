5ad896bcf5ab5d660a0671c27d0a27ac51ee0bfc
sindhu819 <sindu819@gmail.com>
2019-10-14 10:22:21 -0700

    Update Minimum element.py

--- Minimum element.py
+++ Minimum element.py
@@ -1,23 +1,22 @@
 # Time complexity : o(logn)
 #space complexity : o(1)
 #yes my code ran on IDE
-# Approach : Using binary search implementaion. Basic concept behind this is finding the arr[mid]-mid!=arr[low]-low. Then we adjust the low and high values accordingly. In the end we left out with two elements. Then we find the missing element by calculating the (arr[low] +arr[high])//2
+# Approach : Using binary search implementaion. Basic concept behind this is finding the arr[mid]-mid!=arr[low]-low. 
+Then we adjust the low and high values accordingly. In the end we left out with two elements. 
+Then we find the missing element by calculating the (arr[low] +arr[high])//2
 
 
 
 def missing(arr):
     low=0
     high=len(arr)-1
-    mid=0
     while (high-low)>=2:
         mid=low+(high-low)//2
-        mid_diff=arr[mid]-mid
-        low_diff=arr[low]-low
-        high_diff=arr[high]-high
-        if (mid_diff-low_diff)!=0: #logic
+        if (arr[mid]-arr[low])!=(mid-low):
             high=mid
         else:
             low=mid
-    return (arr[low]+arr[high])//2 # returning the missing element
+    return (arr[low]+arr[high])//2
+
 
 
