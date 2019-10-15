a9fcb5579aac6bd0091a35b92ab8edd485dfe5c2
Shridhar Saurin Shah <42818678+Shridhar-2205@users.noreply.github.com>
2019-10-14 18:26:31 -0700

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,21 @@
 
+public class Main {    public static void main(String[] args) {
+      System.out.println("Hello World");
+      int arr[]={1,2,3,4,5,6,7,8};       System.out.println(binSearch(arr));    }
+   public static int binSearch(int arr[]){
+      int l=0;
+      int h=arr.length-1;       while(l<=h){
+           int mid=l+(h-l)/2;
+          if(arr[mid]==mid+1) {
+              l=mid+1;
+          }
+          else{
+              h=mid-1;               if(mid==0 || arr[mid-1]-mid==0 ){                   return mid+1;
+              }
+          }
+      }
+   return -1;
+  }
+}
+//TC:O(logn)
+//SC:O(1)
