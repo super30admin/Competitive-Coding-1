5f49647b2db698a191aaf5fe11c2645f3dd89115
pnandini <44413220+pnandini@users.noreply.github.com>
2019-10-15 13:10:45 -0700

    Add files via upload

--- problem 11.py
+++ problem 11.py
@@ -1,3 +1,22 @@
+class optimal_sol():
+    # the logic here is that if the numbers  are sorted and not missing any element inbetween then
+    #  the difference between numbers and  their indices shoudl be same
+    # ex: a=[1,2,3,4,5,6,7], low =0, high =6    mid element is a[3]=4 and a[low]=1 and a[high] = 7
+    #a[3]-3=1 and a[4]-4=1 and a[0]-1=1
+    def missing_number(self,a):
+        low= 0
+        high = len(a)-1
+
+        while high - low >=2 : #search until 2 numbers are
+            mid = (low + high) // 2
+            middiff = a[mid] - mid
+            lowdiff = a[low]-low
+            #highdiff = a[high]-high
+            if (middiff != lowdiff): #left part has missing number
+                high=mid
+            else:
+                low=mid #right part has missing number
+        return (a[low] + a[high])/2
 class Solution(object):
     def missingNumber(self, nums):
         """
@@ -9,4 +28,15 @@ class Solution(object):
         #print(self.n,len(nums))
         sum_n=(self.n*((self.n)+1))/2
         sum_arr = sum(self.nums)
+        dif = sum_n - sum_arr
+        return dif
         #print(sum_n,sum_arr)
+
+
+
+a=[2,3,4,6,7,8,9]
+f= optimal_sol()
+print(f.missing_number(a))
+
+
+
