9dc1d6fe30a2ca2088d45de0aa11f480cbdc69d9
Neha Sharma <nsharma335@gmail.com>
2019-11-25 01:55:36 -0800

    Updated Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,19 @@
 
+//Time Complexity : O(n)
+//space : O(1)
+//leetcode : yes
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        //check what should be the actual sum of n integers
+        int sum= n*(n+1)/2;
+        //calculate current array sum and subtract from orignal
+        for(int i=0;i<n;i++)
+        {
+            sum-=nums[i];
+            
+        }
+        return sum;
+    }
+}
