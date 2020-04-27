8b4269e8c91d3840e0b84d31dd6b9c583c73a550
Ravi <aksravi@deloitte.com>
2020-04-27 13:18:25 +0530

    Completed finding missing element problem in O(n)

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        //int n=nums.size()-1;
+        int s=(nums.size()*(nums.size()+1))/2;
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+            sum+=nums[i];
+        return s-sum;
+    }
+};
