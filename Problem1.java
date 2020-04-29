8c7fb24aea3568a4df440b909954b57e88c9d74d
Raju Datla <rajudatla@Rajus-MacBook-Pro.local>
2020-04-28 19:34:02 -0700

    completed exercise

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,36 @@
+//Time Complexity: o(logn)
+//space complexity o(1)
 
+public class MissingNumber{
+    
+    public int CalMissingNumber(int[] arr){
+        
+        if(arr == null || arr.length==0)
+        return -1;
+        
+        int low = 0, high = arr.length-1;
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(mid == 0 || (arr[mid]-arr[mid-1]!=1))
+                {
+                    return arr[mid]-1;
+                }
+            if(mid == arr.length-1 || (arr[mid+1]-arr[mid]!=1))
+                {
+                    return arr[mid]+1;
+                }
+            if((mid-low)!=(arr[mid]-arr[low]))
+                high = mid -1;
+            else if((high-mid)!=(arr[high]-arr[mid]))
+                low = mid + 1;
+        }
+        return -1;
+    }
+
+     public static void main(String []args){
+         int[] arr ={1,2,3,4,6,7,8};
+         MissingNumber m = new MissingNumber();
+        System.out.println("Missing Number: "+ m.CalMissingNumber(arr));
+     }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,89 @@
 
+//Min heap
+//Time complexity for getting min element is o(1), extractMin o(logn), insertion o(logn)
+//space complexity o(1)
+
+public class MinHeap{
+    
+    private int[] heap;
+    private int size;
+    private int maxSize;
+    private static final int TOP = 1;
+    
+    public MinHeap(int maxSize){
+        this.maxSize = maxSize;
+        size = 0;
+        heap = new int[maxSize + 1];
+        heap[0] = Integer.MIN_VALUE;
+    }
+    
+    private void MinHeapify(int pos){
+        if(pos >= size/2 && pos<=size)
+            return;
+            
+        if(heap[pos] > heap[2*pos] || heap[pos] > heap[2*pos + 1]){
+            if(heap[2*pos] < heap[2*pos + 1])
+            {
+                swap(pos,2*pos);
+                MinHeapify(2*pos);
+            }
+            else{
+                swap(pos,2*pos+1);
+                MinHeapify(2*pos+1);
+            }
+        }
+            
+    }
+    
+    private void swap(int a, int b){
+        int temp = heap[a];
+            heap[a] = heap[b];
+            heap[b] = temp;
+    }
+    
+    private void insert(int num){
+        if(size >= maxSize)
+            return;
+        heap[++size] = num;
+        int curpos = size;
+        //according to heap property, parent element shoeld be less than child elements
+        while(heap[curpos]<heap[parent(curpos)]){
+            //swapping of elements
+            swap(curpos,parent(curpos));
+            
+            curpos = parent(curpos);
+        }
+    }
+    
+    private int parent(int pos){
+        return pos/2;
+    }
+    
+    public int extractMin(){
+        int min = heap[TOP];
+        heap[TOP] = heap[size--];
+        MinHeapify(TOP);
+        
+        return min;
+    }
+    
+    private void minheap(){
+        for(int i = size/2; i>=1; i--){
+            MinHeapify(i);
+        }
+    }
+
+     public static void main(String []args){
+         MinHeap m = new MinHeap(10);
+         m.insert(8);
+         m.insert(3);
+         m.insert(18);
+         m.insert(32);
+         m.insert(6);
+         m.insert(20);
+         m.insert(1);
+         m.minheap();
+        System.out.println("min Number: "+ m.extractMin());
+        System.out.println("min Number: "+ m.extractMin());
+     }
+}
