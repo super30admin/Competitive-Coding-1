94fb063f8cc207324839c2e3e949f1faf02d50e0
OMKAR POTREKAR <39078516+omkarpotrekar@users.noreply.github.com>
2020-05-31 14:31:49 -0400

    Completed Problem# 14

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,1 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        int missingNum = 0;
+        for(int i=0; i < nums.size(); i++)
+            missingNum ^= (nums[i] ^ i);
+        
+        return missingNum ^ nums.size();
+    }
+};
+
 
