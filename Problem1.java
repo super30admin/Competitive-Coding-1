c232b0a6c0df344d2d2475ce1255da5c8db3c76f
padma.cheruvu <padmapriya_cheruvu@yahoo.in>
2020-06-01 17:29:03 -0500

    Competitive coding commit

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,37 @@
+// Time Complexity : O(log n) where n is array length
+// Space Complexity :O(1) 
+// Did this code successfully run on Leetcode : Not on leetcode
+// Any problem you faced while coding this : No
 
+public class Problem1 {
+
+	public static void main(String[] args) {
+		int[] array = {1,2,3,4,6,7,8,9,10,11,12,13}; //5 missing
+		System.out.println(search(array));
+	}
+	
+	public static int search(int[] array)
+	{
+		if(array.length==0) return -1;
+		
+		int low = 0;
+		int high = array.length-1;
+		if(low+1!=array[low]) return low+1;
+		if(high+1==array[high]) return array[high]+1;
+		
+		while(low < high)
+		{
+			int mid = low + (high-low)/2;
+			
+			if(mid+1 != array[mid])
+			{
+				high = mid;
+			}
+			else
+			{
+				low = mid + 1;
+			}
+		}
+			return low+1;
+	}
+}
