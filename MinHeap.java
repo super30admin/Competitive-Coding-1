9cdd69f8bd9cbfb358de15907ea75d414c4be06a
Aditya Mathur <adityamathur97@gmail.com>
2020-07-13 22:44:35 -0500

    Completed the Exercise

--- MinHeap.java
+++ MinHeap.java
@@ -5,10 +5,12 @@ insert() - O(log(n))
 
 //Space Complexity - O(n)
 
+package axm180195;
+
 public class MinHeap {
-	int capacity;
+	int capacity; // capacity of minheap
 	Integer[] li;
-	int index;
+	int index; // index of last element + 1
 
 	public MinHeap() {
 		capacity = 10;
@@ -17,23 +19,29 @@ public class MinHeap {
 	}
 
 	private int getMin() {
-		return li[0];
+		return li[0]; // return the top element
 	}
 
 	private void extractMin() {
-		li[0] = li[index - 1];
-		li[index - 1] = null;
-		perculateDown(0);
+		li[0] = li[index - 1]; // replace last element with top
+		if (index - 1 > 0) { // and move it to its correct position
+			li[index - 1] = null;
+			index--;
+			perculateDown(0);
+		}
 	}
 
 	private boolean insert(int x) {
+		if (index >= capacity) // if more than capacity then increase capacity
+			remake();
 		li[index++] = x;
 		int child = index - 1;
 		int parent = child / 2;
-		perculateUp(parent, child);
+		perculateUp(parent, child); // moving element up if its smaller
 		return true;
 	}
 
+	// helper function to move element up if its smaller
 	private void perculateUp(int parent, int child) {
 		while (li[parent] > li[child] && parent >= 0) {
 			int temp = li[parent];
@@ -44,6 +52,7 @@ public class MinHeap {
 		}
 	}
 
+	// helper function to move element up if its larger
 	private void perculateDown(int parent) {
 		int left = (2 * parent) + 1;
 		int right = left + 1;
@@ -56,26 +65,30 @@ public class MinHeap {
 			else
 				index = right;
 
-			swap(parent, index);
-
-			if (li[parent + 1] < li[parent]) {
-				swap(parent, parent + 1);
-			}
-
-			parent = index;
-			left = (2 * parent) + 1;
-			right = left + 1;
-		}
-
-		if (li[parent + 1] < li[parent]) {
-			swap(parent, parent + 1);
+			if (li[index] < li[parent]) {
+				swap(parent, index);
+				parent = index;
+				left = (2 * parent) + 1;
+				right = left + 1;
+			} else
+				break;
 		}
 	}
 
+	// swaps two numbers in array
 	private void swap(int i, int j) {
 		int temp = li[i];
 		li[i] = li[j];
 		li[j] = temp;
+	}
+
+	// remake the MinHeap with twice capacity
+	private void remake() {
+		capacity = capacity * 2;
+		Integer[] temp = li;
+		li = new Integer[capacity];
+		for (int i = 0; i < temp.length; i++)
+			li[i] = temp[i];
 	}
 
 	public static void main(String[] args) {
@@ -90,7 +103,12 @@ public class MinHeap {
 		System.out.println(heap.getMin());
 		heap.extractMin();
 		System.out.println(heap.getMin());
+		heap.extractMin();
+		System.out.println(heap.getMin());
+		heap.extractMin();
+		System.out.println(heap.getMin());
 	}
 }
+
 
 
