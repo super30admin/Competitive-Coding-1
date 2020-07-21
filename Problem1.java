6c6ea69ac6140a9d3c50cce02ec6ad539bbb2d32
Sarthak Goel <goel.sar@husky.neu.edu>
2020-07-20 21:45:17 -0400

    Added the code for Find Min Element

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,27 @@
+// Time Complexity : O(log N) where N is the number of elements in the Array
+// Space Complexity : O(1)
 
+class Solution{
+	public static int getMinimum(int[] arr){
+		int low = 0;
+		int high = arr.length-1;
+		while(high-low>2){
+			int mid = low + (high-low)/2;
+			int midIndex = arr[mid] - mid;
+			int highIndex = arr[high] - high;
+			int lowIndex = arr[low] - low;
+
+			//Left Sorted
+			if(lowIndex == midIndex) return start = mid;
+			//Right Sorted
+			if(highIndex == midIndex) return end = mid;
+		}
+
+		return (arr[high]-arr[low])/2;
+	}
+
+	public static void main(String[] args){
+		int[] arr = {1,2,3,4,5,6,8,9};
+		System.out.println(getMinimum(arr));
+	}
+}
