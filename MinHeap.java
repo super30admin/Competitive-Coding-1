29b3b60c8cb8e7a3dedf18dcf422f367e3767305
Ganesh Kate <ganesh.kate@walmartlabs.com>
2020-10-20 21:55:13 -0700

    Completed all the problems

---
+++ MinHeap.java
@@ -0,0 +1,99 @@
+public class MinHeap {
+
+    private int[] Heap;
+    private int size;
+    private int maxsize;
+    private static final int FRONT = 1;
+
+    public MinHeap(int maxsize)
+    {
+        this.maxsize = maxsize;
+        this.size = 0;
+        Heap = new int[this.maxsize + 1];
+        Heap[0] = Integer.MIN_VALUE;
+    }
+
+
+    public void insert(int element)
+    {
+        if (size >= maxsize) {
+            return;
+        }
+        Heap[++size] = element;
+        int current = size;
+
+        while (Heap[current] < Heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    public int extractMin()
+    {
+        int popped = Heap[FRONT];
+        Heap[FRONT] = Heap[size--];
+        minHeapify(FRONT);
+        return popped;
+    }
+
+    public int getMin(){
+        return Heap[FRONT];
+    }
+
+    private int parent(int pos)
+    {
+        return pos / 2;
+    }
+
+    private void swap(int fpos, int spos)
+    {
+        int tmp;
+        tmp = Heap[fpos];
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
+    }
+
+    private void minHeapify(int pos)
+    {
+
+        // If the node is a non-leaf node and greater
+        // than any of its child
+        if (!isLeaf(pos)) {
+            if (Heap[pos] > Heap[leftChild(pos)]
+                    || Heap[pos] > Heap[rightChild(pos)]) {
+
+                // Swap with the left child and heapify
+                // the left child
+                if (Heap[leftChild(pos)] < Heap[rightChild(pos)]) {
+                    swap(pos, leftChild(pos));
+                    minHeapify(leftChild(pos));
+                }
+
+                // Swap with the right child and heapify
+                // the right child
+                else {
+                    swap(pos, rightChild(pos));
+                    minHeapify(rightChild(pos));
+                }
+            }
+        }
+    }
+
+    private int leftChild(int pos)
+    {
+        return (2 * pos);
+    }
+
+    private int rightChild(int pos)
+    {
+        return (2 * pos) + 1;
+    }
+
+    private boolean isLeaf(int pos)
+    {
+        if (pos >= (size / 2) && pos <= size) {
+            return true;
+        }
+        return false;
+    }
+}
