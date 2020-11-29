2887cfa6f6fc05e7c27fb28484bfe5e67d415b48
Sarvesh Kulkarni <30865213+snk95@users.noreply.github.com>
2020-11-28 21:36:59 -0800

    done

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,68 @@
+/*
+https://www.geeksforgeeks.org/find-the-missing-number-in-a-sorted-array/
+Provide an array of n-1 integers in the range of 1 to n with no duplicates in list, One of the integers is missing in the list. Write a code to find the missing integer.
 
+Examples:
+Input : arr[] = [1, 2, 3, 5, 6, 7, 8]
+Output : 4
+
+Input : arr[] = [1, 2, 4, 5, 6, 7, 8, 9]
+Output : 3
+*/
+/*
+
+def search(ar, size):
+    if ar is None or len(ar) == 0:
+        return -1
+    low = 0
+    high = len(ar) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if ar[mid] == mid + 1:
+            low = mid + 1
+        else:
+            high = mid - 1
+            
+    return low + 1
+    
+    
+    
+
+# Driver Code 
+a = [] 
+n = len(a) 
+print("Missing number:", search(a, n))
+
+*/
+
+// Time Complexity : O(logn) where n is size of array
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : N/a
+// Any problem you faced while coding this : No
+
+// Your code here along with comments explaining your approach: I have used the logic that number should be equal to its index+1. If
+// it not then there is a number missing
+class Solution {
+    public int search(int[] nums) {
+        if(nums == null || nums.length == 0)
+        return -1;
+
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] == mid+1)
+                low = mid + 1;
+            else
+                high = mid - 1;
+            
+        }
+
+        return low + 1 ;
+        
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,99 @@
+// https://www.geeksforgeeks.org/min-heap-in-java/
 
+// Time Complexity : O(logn) as building heap takes logn time 
+// Space Complexity : O(m) where m is maxsize of heap
+// Did this code successfully run on Leetcode : N/a
+// Any problem you faced while coding this : Yes
+
+// Your code here along with comments explaining your approach: I have used array to store elements. The property that parents start from
+// index 1 and go till size/2. I have purposely put mininum integer at index 0 as even and odd size gives different indexes of parent
+// if we start from index 0. 
+class Minheap{
+    int[] Heap;
+    int size;
+    int maxsize;
+    private static final int START = 1; 
+
+    public Minheap(int maxsize){
+        this.maxsize = maxsize;
+        this.size = 0;
+        Heap = new int[this.maxsize];
+        Heap[0] = Integer.MIN_VALUE;
+    }
+
+    private int parent(int position) {
+        return position / 2;
+    }
+
+    private int leftchild(int position) {
+        return (2*position);
+    }
+
+    private int rightchild(int position) {
+        return (2*position) + 1;
+    }
+
+    private boolean isleaf(int position) {
+        if (position > size/2 && position <= size)
+            return true;
+
+        return false;
+    }
+
+        // Time and space - O(1)
+    private void swap(int first, int second){
+        int temp;
+        temp = Heap[first];
+        Heap[first] = Heap[second];
+        Heap[second] = temp;
+    }
+
+    // Time - O(logn) Space - O(1)
+    private void minheapify(int position) {
+        if (!isleaf(position)){
+            if (Heap[position] > Heap[leftchild(position)] || Heap[position] > Heap[rightchild(position)]){
+                if (Heap[leftchild(position)] < Heap[rightchild(position)]) {
+                    swap(position, leftchild(position));
+                    minheapify(leftchild(position));
+                }
+                else{
+                    swap(position, rightchild(position));
+                    minheapify(rightchild(position));
+                }
+            }
+        }
+    }
+
+    // Time - O(logn) Space - O(m) where m is maxsize
+    public void insert(int ele) {
+        if (size >= maxsize)
+            return;
+
+        size = size + 1;
+        Heap[size] = ele;
+
+        int cur = size;
+        while (Heap[cur] < Heap[parent(cur)]){
+            swap(cur, parent(cur));
+            cur = parent(cur);
+        }
+    }
+
+    // Time - O(logn) Space - O(1)
+    public void minheap() {
+        for(int i=(size/2); i>=1; i--)
+            minheapify(i);
+    }
+
+    // Time - O(logn) Space - O(1)
+    public int remove() {
+        int removed = Heap[START];
+        Heap[START] = Heap[size];
+        size = size - 1;
+        minheapify(START);
+        return removed;
+    }
+
+    
+
+}
