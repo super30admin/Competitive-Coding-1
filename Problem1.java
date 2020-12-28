8dfc9422ee4100ce819f30851f80bc09d7d6a0d6
Lohita Gundapaneni <lohitagundapaneni@Lohitas-MacBook-Air.local>
2020-12-28 16:50:33 -0500

    Problem1-missing integer done

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,36 @@
+import java.io.*; 
 
+class Solution
+{ 
+static int search(int ar[], 
+				int size) 
+{ 
+	int low = 0, high = size - 1; 
+	if(ar[0]!=1)
+	    return 1;
+	if(ar[high] == size)
+	    return size+1;
+	int mid = 0; 
+	while ((high - low) > 1) 
+	{ 
+		mid = (high-low) / 2 + low; 
+		if(ar[mid]-mid == 1) 
+			low = mid;
+		else
+		    high = mid;
+	} 
+	return (ar[mid] + 1); 
+} 
+
+// Driver Code 
+public static void main (String[] args) 
+{ 
+	int ar[] = { 2, 3, 4, 5, 6, 7}; 
+	int size = ar.length; 
+	System.out.println("Missing number: " + 
+						search(ar, size)); 
+} 
+} 
+
+//Time complexity : O(logN) where N is the size of array
+//Space complexity : O(1) since no extra space is used
