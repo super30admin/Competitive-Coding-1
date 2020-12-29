40597b109b1713648519e51a4548b6789e864c6d
abhisheksharma0904 <76174342+abhisheksharma0904@users.noreply.github.com>
2020-12-29 23:33:15 +0530

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,28 @@
 
+// TC = O(log n) where n is number of elements in array.
+// SC = O(1) 
+
+
+class Solution {
+    public int missingElement(int[] arr) {
+            if (arr == null || arr.length == 0) return -1;
+            int low = 0, high = arr.length - 1, diff = arr[0];
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (arr[mid] != mid + diff && mid > 0)
+                {
+                return mid + diff;
+                }
+                else if (arr[mid] == mid + diff && mid < arr.length)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            return -1;      
+    }
+}
