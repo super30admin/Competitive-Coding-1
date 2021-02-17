1f3fa7b863ee189c56cb6583a7e43af0a72e2455
Harsha Vardhan <harshavardhan@harshas-air.3500.dhcp.asu.edu>
2021-02-17 14:38:10 -0700

    cc 1

---
+++ find_missing_number.py
@@ -0,0 +1,11 @@
+# O(log(n)) time and O(1) space where n is length of array
+def missingNumber(array):
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if array[mid] - mid != 1:
+            right = mid
+        else:
+            left = mid + 1
+    return left + 1
