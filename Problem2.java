f55cf55a31b17d35af7d2a834b9ba29a2704b806
Lokeswari Umakanthan <lokeswari.umakanthan@utdallas.edu>
2021-02-24 21:09:19 -0600

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,6 +1,8 @@
 // Find the Missing Number in a sorted array
 // Time Complexity : O(log n)
 // Space complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 class Solution {
     public int missingNumber(int[] nums) {
