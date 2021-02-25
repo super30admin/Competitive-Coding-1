5b86899b7558d08a88232e431349406d241743f2
Lokeswari Umakanthan <lokeswari.umakanthan@utdallas.edu>
2021-02-24 21:08:21 -0600

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,19 @@
+// Find the Missing Number in a sorted array
+// Time Complexity : O(log n)
+// Space complexity : O(1)
 
+class Solution {
+    public int missingNumber(int[] nums) {
+        if(nums.length == 0) return 0;
+        int left = 0,right = nums.length-1;
+      // Using binary search to compare if the elements and their index match
+      // and find the missing number using the index
+        while(left<=right){
+            int mid = left+(right -left)/2;
+            if(nums[mid] != mid)
+                right = mid-1;
+            else left = mid+1;
+        }
+        return left;
+    }
+}
