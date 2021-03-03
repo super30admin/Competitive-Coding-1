5380dcc22fd7a3696e0e85b4cae803048c7afad0
omkar <omkarvaidya13@gmail.com>
2021-03-03 07:28:31 -0500

    Completed Competitive-Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,25 @@
+class Problem1{
 
+    // Time Complexity: O(log n)
+    // Space Complexity: O(1)
+
+    public static int missingNumber(int[] arr){
+        if(arr == null || arr.length == 0)
+            return 1;
+        if(arr[0] > 1)
+            return 1;
+        if(arr[arr.length-1] == arr.length)
+            return arr.length+1;
+
+        int low = 0;
+        int high = arr.length - 1;
+        while((high-low) > 1){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] > (mid+1))
+                high = mid;
+            else
+                low = mid;
+        }
+        return arr[low]+1;
+    }
+}
