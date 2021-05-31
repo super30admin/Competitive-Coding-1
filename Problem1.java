1ff0ba137ac84a8d047522a8156848b3341bd5ac
Harsha Deshmukh <harshadeshmukh859@gmail.com>
2021-05-30 22:33:33 -0700

    Completed Problem-1 Competitive-Coding-1, please review

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,36 @@
 
+import java.io.*;
+
+class Main {
+    
+    public int findMissing(int[] nums){
+        
+        int lo = 0 ;
+        int hi  = nums.length - 1;
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            // check if indices correspond to the actual  contained element at that index
+            if (nums[mid] == mid+1){
+                // if index is consistent with its corresponding element.then continue moving right 
+                lo = mid+1;
+            } else {
+                // index is inconsistent with the elememt, there was an elemnt missing earlier, go to left
+                hi = mid-1;
+            }
+            
+        }
+        return lo+1;
+    }
+    
+    
+	public static void main (String[] args) {
+		System.out.println("Found the first missing number in the given array ");
+		// test case 1 int[] nums = new int[] {1,2,4,5,6,7,8,9};
+		// test case 2 int[] nums = new int[] {1,2,3,4,5,6,8,9};
+		// test case 3 
+		int[] nums = new int[] {1,2,3,4,5,6,7, 8,9};
+		Main mainObj = new Main();
+		int missingNumber = mainObj.findMissing(nums);
+		System.out.println(missingNumber);
+	}
+}
