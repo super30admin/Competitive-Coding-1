c5d0cf7dc7d356dddafcfda1d850a89fb4edb7b4
Divya <divthaz@amazon.com>
2021-06-01 15:03:38 -0700

    Completed Comptitive coding 1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,24 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Problem not in Leetcode
+// Any problem you faced while coding this : 
 
+class Solution {
+    public int missingElement(int[] nums, int k) {
+        int low = 0;
+        int high = k -1;
+
+        while(high - low > 1){
+            int mid = low + (high - low)/2;
+            int lowVal = nums[low] - low;
+            int midVal = nums[mid] - mid;
+            int highVal = nums[high] - high;
+
+            if( lowVal == midVal){
+                low = mid;
+            } else
+                high = mid;
+        }
+        return  nums[low] + 1;
+    }
+}
