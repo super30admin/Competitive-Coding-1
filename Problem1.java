4e42cf739a9196a2b950b88ca9dbd4bbec20e7bb
Yash Dhawan <84486109+yashdhawan-FSD@users.noreply.github.com>
2021-06-01 18:23:20 -0400

    Missing element solution

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,27 @@
+class Problem1{
+    public int missingElement(int[] arr, int size){
+        int low=0;
+        int high=size-1;
+        while(low+1 !=high){
+           int mid= low+(high-low)/2;
+           if(arr[low]-low!=arr[mid]-mid){
+               high =mid;
+           }
+           else if(arr[low]-low!=arr[mid]-mid){
+               low=mid;
+           }
+           }
+           return arr[high]-arr[low];
+    }
+}
 
+class Main{
+    public static void main (String args[])
+{
+    int ar[] = { 1, 2, 3, 4, 5, 6, 8 };
+    int size = ar.length;
+    Problem1 p1 = new Problem1();
+    System.out.println("Missing number: " +
+    p1.missingElement(ar, size));
+}
+}
