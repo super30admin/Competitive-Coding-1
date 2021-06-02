b17e161a13b80f36f2d0c1c8c172c050b5c472fd
Tanreet Dhaliwal <tdhali@uwo.ca>
2021-06-01 20:24:00 -0400

    finished competitive coding

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,21 @@
+public class Problem1 {
 
+    //Space complexity O(1)
+    //Time complexity O(logn)
+    public int search(int[] arrary, int size) {
+        int low = 0;
+        int high = size - 1;
+        int mid = 0;
+
+        while ((high - low) > 1) {
+            mid = (low + high) / 2;
+            if ((arrary[low] - low) != array[mid] - mid) {
+                high = mid;
+            } else if ((arrary[high] - high) != (arrary[mid] - mid)) {
+                low = mid;
+            }
+        }
+        return arrary[low] + 1;
+    }
+
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,22 @@
+public class Problem2 {
 
+    int[] heap;
+    int size;
+    int maxSize;
+
+    //I did not quite understand this problem implementation so I will ask extra help from the TAs this week
+    public Problem2(int maxSize) {
+        this.heap = new int[maxSize];
+        this.size = 0;
+    }
+
+    public insert(int value) {
+        if (this.size <= this.maxSize) {
+            this.heap[this.size - 1] = val;
+            this.size++;
+        } else {
+            this.moveUp(value);
+        }
+    }
+
+}
