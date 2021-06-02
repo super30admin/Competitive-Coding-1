61bfd3b5e06b956a8839ea28e823639613596148
Sri Devi Rella <58195873+sridevirella@users.noreply.github.com>
2021-06-01 19:37:26 -0500

    Competitive-Coding-1 Completed

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,58 @@
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
 
+import java.util.*;
+
+class MissingNumber {
+
+   // with binay search approch
+    public int missingNumberWithBinarysearch(int[] numArray) {
+    
+       int low = 0, high = numArray.length-1;
+        int missingNumber = -1;
+        
+        while(low <= high) {
+            
+           int mid = low + (high - low) / 2;
+            
+            if(numArray[mid]-mid == 1 )
+               return numArray[mid]-1;
+                    
+            else if(numArray[mid]-mid == numArray[low]-low)
+                low = mid;
+            
+            else if(numArray[mid]-mid == numArray[high]-high)
+               high = mid;
+                          
+        }
+        return -1;
+    }
+
+//with hashset
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+ public int missingNumberWithHashSet(int[] numArray) {
+    
+        int expectedCount = numArray.length + 1;
+        Set<Integer> set = new HashSet<Integer>();
+        
+        for (int i=0; i <= numArray.length-1; i++) {
+         set.add(numArray[i]);
+        }
+       
+       for (int j = 0; j < expectedCount; j++) {
+            if (!set.contains(j)) {
+                return j;
+            }
+        }
+        return -1;
+    }
+    
+
+    public static void main(String[] args) {
+    MissingNumber mnObj =  new MissingNumber();
+    int[] numArray = {0,1,2,3,5,6};
+    System.out.println("with Hash Set:" + mnObj.missingNumberWithHashSet(numArray));
+    System.out.println("with binary search:" + mnObj.missingNumberWithBinarysearch(numArray));
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,53 @@
+//Time Complexity: Insertion O(logn), getMin O(1), extractMin O(logn)
+//Space Complexity: O(n)
+
+import java.util.PriorityQueue;
+
+public class MinHeap {
+
+    static PriorityQueue<Integer> priorityQueue;
+
+    public MinHeap() {
+        priorityQueue = new PriorityQueue<>();
+    }
+
+    //the most priority/root element of the heap
+    public static int getMin() {
+        return priorityQueue.peek();
+    }
+
+    //Remove the most priority/root element of the heap and does heapification
+    public static int extractMin() {
+        return priorityQueue.poll();
+    }
+    
+    public static void insert(int element) {
+        priorityQueue.add(element);
+    }
+
+    public static void printMinHeap() {
+
+        for (Integer integer : priorityQueue)
+            System.out.println(integer);
+    }
+    public static void main(String[] args) {
+        MinHeap minHeap = new MinHeap();
+        minHeap.insert(10);
+        minHeap.insert(7);
+        minHeap.insert(6);
+        minHeap.insert(12);
+        minHeap.insert(17);
+        minHeap.insert(5);
+        minHeap.insert(15);
+
+        System.out.println("Min Heap\n");
+        minHeap.printMinHeap();
+        System.out.println("Get Min\n");
+        System.out.println(minHeap.getMin());
+        System.out.println("extract Min\n");
+        System.out.println(minHeap.extractMin());
+        System.out.println("Min Heap\n");
+        minHeap.printMinHeap();
+    }
+}
 
