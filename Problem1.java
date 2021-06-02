f43b0f615c5c1fc7dc78fc97f10944f76467c3dc
Dhanashree Kamath <dhanashreekamath92@gmail.com>
2021-06-02 16:52:27 -0700

    completed Competitive coding

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,34 @@
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+public class Problem1 {
+
+    //finding missing element in given array
+    public static int searchMissingElement(int arr[]) {
+        int low = 0; int high = arr.length-1;
+        // check the index is one less than element in first and last index
+        //if it is then no element is missing
+        if(arr[0]-1 == low && arr[high]-1 == high) {
+            System.out.println("All elements are there");
+            return -1;
+        }
+        // reduce the search space by finding the difference between an index and element is greater than 2 
+        while(low<=high) {
+             int mid = low +(high-low)/2;
+             if(arr[mid] == mid+1) {
+                 low = mid+1;
+             } else {
+                 high = mid-1;
+             }
+        }
+        return low+1;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] arr = new int[] {1,2,4,5};
+        System.out.println("Missing element is " + searchMissingElement(arr));
+    }
+}
 
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,113 @@
+// Time Complexity : insert and remove O(logn) , getMin - O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : 
+// Any problem you faced while coding this : No
+
+public class Program2 {
+    private int[] heap;
+    private int size;
+    private int maxSize;
+    private static final int FRONT = 1;
+
+    public Program2(int maxSize) {
+        this.heap = new int[maxSize+1];
+        this.size = 0;
+        this.maxSize = maxSize;
+        heap[0] = Integer.MIN_VALUE;
+    }
+
+    //find the position of parent and its children
+    public int parent(int index) {
+        return index/2;
+    }
+    private int leftChild(int index) {
+        return (2*index);
+    }
+    private int rightChild(int index) {
+        return (2*index)+1;
+    }
+    //find the node is leaf node
+    private boolean isLeafNode(int index) {
+        if(index >= (size/2) && index<= size) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    //swap two elements two maintain min heap
+    private void swapElements(int firstIndex, int secondIndex) {
+        int temp = heap[firstIndex];
+        heap[firstIndex] = heap[secondIndex];
+        heap[secondIndex] = temp;
+    }
+
+    private void minHeapify(int index) {
+        if(!isLeafNode(index)) {
+            if(heap[index] > heap[leftChild(index)] || heap[index] > heap[rightChild(index)]){
+                //if the current element is greater than left or its right child then swap and current and left child
+                if(heap[leftChild(index)] < heap[rightChild(index)]) {
+                    swapElements(index,leftChild(index));
+                    minHeapify(leftChild(index));
+                } else {
+                    //swap right child
+                    swapElements(index,rightChild(index));
+                    minHeapify(rightChild(index));
+                }
+            }
+        }
+    }
+
+    //remove element
+    public int removeElement() {
+        int poppedElement = heap[FRONT];
+        heap[FRONT] = heap[size--];
+        minHeapify(FRONT);
+        return poppedElement;
+    }
+
+    public void insert(int element) {
+        if(size >= maxSize) {
+            return;
+        }
+        heap[++size] = element;
+        int current = size;
+        while(heap[current] < heap[parent(current)]) {
+            swapElements(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    public int getMin() {
+        return heap[FRONT];
+    }
+    public void print()
+    {
+        for (int i = 1; i <= size / 2; i++) {
+            System.out.print(" parent : " + heap[i]
+                    + " leftchild : " + heap[2 * i]
+                    + " rightchild :" + heap[2 * i + 1]);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The Min Heap is ");
+        Program2 minHeap = new Program2(15);
+        minHeap.insert(5);
+        minHeap.insert(3);
+        minHeap.insert(17);
+        minHeap.insert(10);
+        minHeap.insert(84);
+        minHeap.insert(19);
+        minHeap.insert(6);
+        minHeap.insert(22);
+        minHeap.insert(9);
+        minHeap.print();
+        minHeap.removeElement();
+        System.out.println("*******after removing element*****");
+        minHeap.print();
+        System.out.println("minimum element "+minHeap.getMin());
+    }
+}
 
