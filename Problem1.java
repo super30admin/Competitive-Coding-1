44fd3cf4da5a0e4f9f44c5985a6fd274aba72d15
Srirag <78050630+sriragcs@users.noreply.github.com>
2021-06-28 12:09:58 -0700

    Completed Competitive Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,35 @@
+// Time Complexity : O(log n) : n -> size of array
+// Space Complexity : O(1) 
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
+public class Problem1 {
+	/*
+	 * Use binary search. If difference between element and indes is 1, then the
+	 * missing element is in the tright half. Else, the missing element is in thew
+	 * left half.
+	 */
+	
+	public int findMissingElement(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return 1;
+		}
+
+		int low = 0, high = nums.length - 1;
+
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			if (nums[mid] - mid == 1) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		return low + 1;
+	}
+
+	public static void main(String[] args) {
+		Problem1 obj = new Problem1();
+		System.out.println("Missing element: " + obj.findMissingElement(new int[] { 1, 2, 3, 4, 6, 7, 8 }));
+	}
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Competitive-Coding-1
+
+## Problem 1:
+Given a sorted array of size n containing numbers from 1 to n+1, find the missing number.
+
+Example:
+Input: [1,2,4,5,6,7,8],
+Output:
+3
+
+Note:
+All inputs will be sorted.
