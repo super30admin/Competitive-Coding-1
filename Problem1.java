dcdc0ae4b0c6dd7c31e46e6548202562a2f3fa4f
Nyrikabr <nyrikabr@gmail.com>
2021-06-29 23:11:39 +0530

    Completed Sprint Competitive-Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,31 @@
+// A binary search based program to find the only missing number in a sorted array of distinct elements within limited range.
+// Time Complexity - O(logN)
+// Space Complexity - O(1)
 
+import java.io.*;
+
+class GFG
+{
+static int search(int ar[],
+				int size)
+{
+	int a = 0, b = size - 1;
+	int mid = 0;
+	while ((b - a) > 1)
+	{
+		mid = (a + b) / 2;
+		if ((ar[a] - a) != (ar[mid] - mid))
+			b = mid;
+		else if ((ar[b] - b) != (ar[mid] - mid))
+			a = mid;
+	}
+	return (ar[a] + 1);
+}
+public static void main (String[] args)
+{
+    int ar[] = { 1, 2, 3, 4, 5, 6, 8 };
+    int size = ar.length;
+    System.out.println("Missing number: " +
+                        search(ar, size));
+}
+}
