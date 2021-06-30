fe9fba2f8c08e2480cbbccc7f9efeb61859b0f1e
Sushmitha B <sushb798@gmail.com>
2021-06-30 16:25:49 -0500

    Competitive Coding 1 completed

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,44 @@
+//Provide an array of n-1 integers in the range of 1 to n with 
+//no duplicates in list, One of the integers is missing in the list. 
+//Write a code to find the missing integer.
+
+//Time Complexity: O(log n)
+//Space Complexity: O(1) 
+//Approach: Binary Search
+
+public static void main(String[] args) {
+		int[] arr = new int[] {1,2,3,5}; 
+
+		int left = 0;
+		int right = arr.length - 1;
+
+        //loop through the array unitil the left pointer < right pointer
+        // in each iteration, calculate the mid value
+        //and check if elements adjacent to mid element are increment or decrement by 1
+        // if so print the missing element and return
+        //else if the number of elements on left side matches the value move left  to mid + 1
+        //else move right to mid - 1
+		while(left < right) {
+			int mid = (right + left )/ 2;
+
+			if(!(arr[mid-1] == (arr[mid] - 1) )) {
+				System.out.println("This is the missing element :"+ (arr[mid]-1)); 
+				return; 
+			}else if(!(arr[mid + 1] == (arr[mid] + 1))) {
+				System.out.println("This is the missing element: "+ (arr[mid] + 1));
+				return;
+			}else if(arr[mid] == mid + 1) {
+				left = mid + 1;
+				System.out.println("left :" + left);
+			}else {
+				right = mid - 1;
+				System.out.println("right: "+ right);
+			}
+
+		}
+
+		System.out.println("NO element is missing");
+	}
+
+}
 
