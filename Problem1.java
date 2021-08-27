c004f5d8765c197e00e6afc89821938b4d8d2f78
Priyansh Patel <43277093+priyanshpatel@users.noreply.github.com>
2021-08-27 13:16:01 -0700

    Completed Sprint Competitive-Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,41 @@
+class Problem1
+{
+    static int search(int ar[], int size)
+    {
+        // Initialize low and high pointers
+        int low = 0;
+        int high = size - 1;
+        while (high - low >= 2){
+            // Initialize mid pointer
+            int mid = low + (high - low) / 2;
+            // Find difference between the pointers and their values
+            int lowIndexDiff = ar[low] - low;
+            int midIndexDiff = ar[mid] - mid;
+            int highIndexDiff = ar[high] - high;
 
+            // If a number is missing between any of two pointers, the diffence
+            // between their corresponding index and value will not be constant
+            if(lowIndexDiff != midIndexDiff){
+                // Number is missing between low and mid
+                // move left
+                high = mid;
+            }
+            else if(highIndexDiff != midIndexDiff){
+                // Number is missing between mid and high
+                // move right
+                low = mid;
+            }
+        }
+        // find the missing number by taking average of values at low and high pointers
+        return (ar[low] + ar[high]) / 2;
+    }
+
+    // Driver Code
+    public static void main (String[] args)
+    {
+        int ar[] = { 1, 2, 3, 4, 5, 6, 8 };
+        int size = ar.length;
+        System.out.println("Missing number: " +
+                search(ar, size));
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,137 @@
+class minHeap {
+    private int[] Heap;
+    private int size;
+    private int maxsize;
 
+    // initialize the top most element of the heap
+    private static final int FRONT = 1;
+
+
+    // constructor method
+    public minHeap(int maxsize)
+    {
+        //initialize maxsize and size variables
+        this.maxsize = maxsize;
+        this.size = 0;
+
+        Heap = new int[this.maxsize + 1];
+        Heap[0] = Integer.MIN_VALUE;
+    }
+
+
+    private int parent(int pos) {
+        // return parent node's index
+        return pos / 2;
+    }
+
+    private int leftChild(int pos) {
+        // return left child node's index
+        return (2 * pos);
+    }
+
+    private int rightChild(int pos)
+    {
+        // return right child node's index
+        return (2 * pos) + 1;
+    }
+
+    private boolean isLeaf(int pos)
+    {
+        // return true of the node is a leaf node
+        if (pos > (size / 2) && pos <= size) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void swap(int fpos, int spos)
+    {
+        // swap two nodes used during heapify
+        int tmp;
+        tmp = Heap[fpos];
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
+    }
+
+    private void minHeapify(int pos)
+    {
+
+        // If the node is a non-leaf node and greatee than any of its child
+        if (!isLeaf(pos)) {
+            if (Heap[pos] > Heap[leftChild(pos)]
+                    || Heap[pos] > Heap[rightChild(pos)]) {
+
+                // Swap with the left child and heapify the left child
+                if (Heap[leftChild(pos)]
+                        < Heap[rightChild(pos)]) {
+                    swap(pos, leftChild(pos));
+                    minHeapify(leftChild(pos));
+                }
+
+                // Swap with the right child and heapify the right child
+                else {
+                    swap(pos, rightChild(pos));
+                    minHeapify(rightChild(pos));
+                }
+            }
+        }
+    }
+
+    public void insert(int element)
+    {
+        if (size >= maxsize) {
+            // return if the parent node capacity is reached
+            return;
+        }
+
+        // insert new element
+        Heap[++size] = element;
+        int current = size;
+
+        // heapify up until the node is greater or equal to its parent node
+        while (Heap[current] < Heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    public void print()
+    {
+        for (int i = 1; i <= size / 2; i++) {
+
+            // Print the heap
+            System.out.print("\nParent node : " + Heap[i] + " Left child node : " + Heap[2 * i] + " Right child node :" + Heap[2 * i + 1]);
+        }
+    }
+
+    public int remove()
+    {
+        // remove and return the least (top-most) element from the heap
+        int popped = Heap[FRONT];
+        Heap[FRONT] = Heap[size--];
+        minHeapify(FRONT);
+
+        return popped;
+    }
+
+    // Driver code
+    public static void main(String[] arg)
+    {
+        minHeap heap = new minHeap(15);
+
+        heap.insert(7);
+        heap.insert(3);
+        heap.insert(19);
+        heap.insert(156);
+        heap.insert(87);
+        heap.insert(29);
+        heap.insert(1);
+        heap.insert(32);
+        heap.insert(2);
+
+        heap.print();
+
+        System.out.println("\nThe minimum val is " + heap.remove());
+    }
+}
