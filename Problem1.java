2b437ede93409df7036a6768cff8879a9682ccc9
hackerrank <me@hackerrank.com>
2021-09-03 20:43:32 -0700

    Completed Sprint Competitive-Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,55 @@
+class MinHeap {
+    private:
+    int size;
+    int capacity;
+    vector<int> minHeap;
+    int parent(int i) { return (i-1)/2; }
+    int left(int i) { return 2*i + 1; }
+    int right(int i) { return 2*i + 2; }
+    public:
+    MinHeap(int capacity);
+    void insert(int k);
+    int extractMin();
+    int getSize();
+    int getElement(int idx);
+};
 
+MinHeap::MinHeap(int capacity) {
+        size = 0;
+        this->capacity = capacity;
+        minHeap.resize(capacity);
+        };
+
+        void MinHeap::insert(int k) {
+        if(size == capacity) return;
+        size++;
+        int i = size-1;
+        minHeap[i] = k;
+
+        while(i != 0 and minHeap[parent(i)] > minHeap[i]) {
+        swap(minHeap[parent(i)], minHeap[i]);
+        i = parent(i);
+        }
+        }
+
+        int MinHeap::extractMin() {
+        if(size == 0) return -1;
+        if(size == 1) {
+        size--;
+        return minHeap[0];
+        } else {
+        int root = minHeap[0];
+        minHeap[0] = minHeap[size-1];
+        size--;
+        heapify(0);
+
+        return root;
+        }
+        }
+
+        int MinHeap::getSize() {return size;}
+
+        int MinHeap::getElement(int idx) {
+        if(idx > size or idx < 0) return -1;
+        return minHeap[idx];
+        }
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,22 @@
 
+//  time complexity: O(logn)
+        //space complexity: O(1)
+
+class missingnumberinsortedarray //performing binary search and finding the missing number
+{
+    static int search(int ar[],
+                      int size)
+    {
+        int a = 0, b = size - 1;
+        int mid = 0;
+        while ((b - a) > 1)
+        {
+            mid = (a + b) / 2; //computing difference and finding the missed element
+            if ((ar[a] - a) != (ar[mid] - mid))
+                b = mid;
+            else if ((ar[b] - b) != (ar[mid] - mid))
+                a = mid;
+        }
+        return (ar[a] + 1);
+    }
+}
