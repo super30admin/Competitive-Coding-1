ba7b749ddb44a5ceec9763c0fa7811f1d129d886
vg4838 <vg4838@g.rit.edu>
2021-10-22 20:27:18 -0400

    CompetitiveCoding 1 - VIvek review

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,30 @@
+package CompetitiveCoding1;
 
+class Problem1 {
+
+    static int search(int ar[],
+
+                      int size) {
+
+        int a = 0, b = size - 1;
+
+        int mid = 0;
+
+        while ((b - a) > 1) {
+
+            mid = (a + b) / 2;
+
+            if ((ar[a] - a) != (ar[mid] - mid))
+
+                b = mid;
+
+            else if ((ar[b] - b) != (ar[mid] - mid))
+
+                a = mid;
+
+        }
+
+        return (ar[a] + 1);
+
+    }
+}
