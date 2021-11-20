67bcc1fa13b45a8c5bf470b8d5af6d128b71e102
hp2413 <hpatel130198@gmail.com>
2021-11-20 08:50:20 -0500

    Comprtitive-Coding 1 - Completed all

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,15 @@
-
+// Time Complexity : O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : Yes
+// Three line explanation of solution in plain english
+// using XOR we can campare the numbers and find the single number
+// Your code here along with comments explaining your approach
+class Solution {
+    public int singleNumber(int[] nums) {
+        int sum = 0;
+        for( int num : nums ){
+            sum ^= num;
+        }
+        return sum;
+    }
+}
