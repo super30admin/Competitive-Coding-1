e00d490dbe8a3346698de89968662692f596597d
Yadnesh Kulkarni <ykulkar2@uncc.edu>
2021-12-01 18:50:59 -0500

    Completed First Problem

--- Problem1.cpp
+++ Problem1.cpp
@@ -8,6 +8,13 @@ using namespace std;
 // Space Complexity : O(1)
 // Problem : Came up with the approach but found some issues while writing the code
 
+//Approach : Find the middle element
+//           Check whether any of the indices of low mid or high have a difference of greater than 1
+//           If low or mid have difference with indices greater than 1 then we can safely assume that
+//           left half of the array has a missing element
+//           If low or mid both have difference of 1 and high has difference greater than 1
+//           then we can assume that missing element lies on the right half of array
+//           Once mid / low / high are at the same position, we can return mid + 1 as missing element  
 int getMissingElement(vector<int> nums)
 {
     int low = 0;
