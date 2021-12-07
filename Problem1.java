9052d03bcd67d8566888f6c96e6e8e29aa9fe7dc
Rishab Semlani <rsemlani@uncc.edu>
2021-12-07 09:40:00 -0500

    Completed! Please Review

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,20 @@
-
+//Time Complexity: O(logn)
+//Space Complexity: O(1)
+class interview{
+    int low = 0;
+    int high = arr[s].length -1;
+    
+    int binarySearch(int arr[]){
+        while(high-low>1){
+        mid = low + (high-low)/2;
+             
+        if(arr[mid]-arr[low]!=mid-low){
+            high=mid;
+        }
+        else{
+            low=mid;
+        }
+    }
+    return (low+high)/2;
+}
+}
