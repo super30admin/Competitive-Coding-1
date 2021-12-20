04adb7af6bc4b115baeed71cc3bdae29471b707b
Jay <dsrujay@gmail.com>
2021-12-19 22:59:45 -0800

    Competitive-Coding-1

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,37 @@
+class Solution{
 
+    Node head;
+    static class Node{
+        int value;
+        int priority;
+        Node next;
+
+        Node(int val, int priority){
+            this.val = val;
+            this.priority = priority;
+            this.next = null;
+        }
+        static void push(int val, int priority){
+            Node temp = new Node(val, priority);
+            if(head == null){
+                head = temp;
+                return;
+            }
+            Node curr = head;
+            if(curr.priority > priority){
+                temp.next = head;
+                head = temp;
+                return;
+            }
+            while(curr.next != null && curr.priority < priority){
+                curr = curr.next;
+            }
+            temp.next = curr.next;
+            curr.next = temp;
+        }
+        static int pop(){
+            Node temp = head;
+            head = head.next;
+            return temp.priority;
+        }
+}
