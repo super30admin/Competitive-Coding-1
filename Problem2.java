5134b5fc1a61fc1f4e11b7f56376e5d308c7db48
AakashDeogaonkar15 <aakashdeogaonkar1594@gmail.com>
2021-12-27 14:46:04 -0800

    Completed both the problems from Competitive-Coding-1

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,24 @@
+import java.util.PriorityQueue;
 
+class Problem2 {
+	PriorityQueue<Integer> minHeap;
+	
+	public Problem2() {
+		minHeap = new PriorityQueue<>();
+	}
+	
+	// O(1)
+	public int getMin() {
+		return minHeap.peek();
+	}
+	
+	// O(log N)
+	public void extractMin() {
+		minHeap.poll();
+	}
+	
+	// O(log N)
+	public void insert(int val) {
+		minHeap.add(val);
+	}
+}
