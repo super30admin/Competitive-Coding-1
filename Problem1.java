110ee39c3ed5048856a2221663f60f3d918bbe4b
ehassah10 <ehassah10@gmail.com>
2021-12-27 23:43:16 -0800

    initial commit

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,36 @@
 
+//Provide an array of n-1 integers in the range of 1 to n with no duplicates in list, One of the integers is missing in the list. Write a code to find the missing integer.
+//Input : arr[] = [5, 2, 3, 1, 6, 7, 8]
+//Output : 4
+
+//Time Complexity: O(N) where N is length of input array
+//Space complexity: O(1)
+
+public class Problem1{
+    public static void main(String args[]){
+        int[] nums = {1, 2, 3, 4, 5, 6, 8, 9};
+        System.out.println(findMisingNumber(nums));
+    }
+
+    private static int findMisingNumber(int[] nums){
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+
+        for(int i=0; i<nums.length; i++){
+            int idx = Math.abs(nums[i]) - 1;
+
+            if(idx < nums.length && nums[idx] > 0){
+                nums[idx] = -1 * nums[idx];
+            }
+        }
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] > 0){
+                return i + 1;
+            }
+        }
+
+        return nums.length;
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,19 @@
+/**
+ * A Min-Heap is a complete binary tree in which the value in each internal node is less than or equal to the values in the children of that node.
+Mapping the elements of a heap into an array is trivial: if a node is stored a index k, then its left child is stored at index 2k + 1 and its right child at index 2k + 2.
 
+Operations on Min Heap:
+    getMin(): It returns the root element of Min Heap. O(1)
+    extractMin(): Removes the minimum element from MinHeap. O(log N) as this operation needs to maintain the heap property after removing root.
+    insert(): We add a new key at the end of the tree. If new key is larger than its parent, then we don’t need to do anything. Otherwise, we need to traverse up to fix the violated heap property. O(log N)
+ */
+
+ public class Problem2{
+     public static void main(String args[]) {
+         
+     }
+
+     private static void name() {
+         
+     }
+ }
