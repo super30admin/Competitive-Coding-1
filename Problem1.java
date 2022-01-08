960674b2ca721faf768184e643cc742d2da01f3d
Ikshaku Goswami <ig752994@gmail.com>
2022-01-08 18:37:19 +0530

    completed missing number

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,29 @@
-
+//missing element in a sorted array
+//tc log n
+//sc 1
+class Solution {
+  
+  public int MissingNumber(int[] arr) {
+    
+    int low = 0;
+    int high = arr.length -1;
+    
+    while(high - low + 1 > 2){
+      
+      int mid = (low + (high - low)) / 2;
+      
+      if(arr[low] - low != arr[mid] - mid) {
+        
+        high = mid;
+        
+      }
+      else if(arr[high] - high != arr[mid] - mid){
+        
+        low = mid;
+        
+      }
+    
+    }
+    return arr[low] + 1;
+  }
+}
