83d28c3199557b063706974583e8fd8ce0195570
akashamin01 <aakashjamin1239@gmail.com>
2022-01-08 20:30:41 -0800

    Completed Competitive-Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,25 @@
+// Time Complexity: O(log(n))
+// Space Complexity: O(1)
 
+
+public class FindMissingNumber{
+    public static int missingNumber(int arr[]){
+        int low = 0;
+        int high = arr.length-1;
+        while(low+1<high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] == mid+1){
+                low = mid;
+            }else{
+                high = mid;
+            }
+        }
+        return arr[low]+1;
+
+    }
+    public static void main(String args[]){
+        int[] arr = {1,2,3,5,6,7};
+        int missing = missingNumber(arr);
+        System.out.print("Missing Number = " + missing);
+    }
+}
