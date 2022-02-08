828d58108012ea2d1dfc8003a82fa1450d441cd5
Parikshit Narang <parikshit3097@gmail.com>
2022-02-07 19:11:04 -0500

    completed competitive-coding-1 : please review

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,16 @@
-
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] arr = new int[]{1, 2, 3, 5, 6, 7, 8};
+	    int n = arr.length;
+	    int sum_n = ((n + 1) * ( n + 2)) / 2;
+	   
+	    int temp = 0;
+	    for(int i=0; i<arr.length; i++){
+	        temp = temp + arr[i];
+	    }
+	    
+	    System.out.println(sum_n - temp);
+	}
+}
