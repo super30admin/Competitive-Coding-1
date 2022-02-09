8baae6f7f2132d930616d7f24fd46540a4aa809c
Jana <janaaharri97@gmail.com>
2022-02-08 18:38:56 -0800

    Completed Interview Problem - Please Review

---
+++ Problem1.py
@@ -0,0 +1,28 @@
+
+
+
+def search(nums):
+
+    s, e = 0, len(nums)-1
+
+    # if no missing element
+    if e-s == nums[e]-nums[s]:
+        return -1
+
+    # if difference of last and first index is greater than or equal to 2
+    while e-s >= 2:
+        m = (s+e) // 2
+
+        # missing element is on the right of mid
+        if nums[m]-nums[s] != m-s:
+            e = m
+        else:
+            s = m + 1
+
+    return e+nums[0] if s == nums[s]-nums[0] else s+nums[0]
+
+
+ 
+# Driver Code
+a = [1, 2, 3, 4, 5, 6, 8]
+print("Missing number:", search(a))
