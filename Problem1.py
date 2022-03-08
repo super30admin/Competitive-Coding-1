bfb29662327f34b370534b6fe86986a13be74aba
Sangeet Asati <sangeet@medshorts.com>
2022-03-07 19:55:36 -0500

    tweak

--- Problem1.py
+++ Problem1.py
@@ -6,7 +6,7 @@
 # Time Complexity: O(logn)
 
 
-#arr = [1,3,4,5,6,7,8,9]
+arr = [1,2,3,5,6,7,8]
 
 start = 0
 end = len(arr) -1
@@ -15,13 +15,13 @@ while(start<=end):
     
     mid = (start + end) // 2
     
-    if arr[end] - arr[mid] == end - mid: #7-4 == 5-2  4 -1 == 2-0
+    if arr[end] - arr[mid] == end - mid:
         if arr[mid] - arr[mid -1] > 1:
-            print arr[mid] - 1
+            print (arr[mid] - 1)
             break
         end = mid - 1
     else:
          if arr[mid -1] - arr[mid] > 1:
-            print arr[mid] - 1
+            print (arr[mid] - 1)
             break
          start = mid + 1
