82f76f3548a16f152d875ffd1d7e15a2848740cb
Jeel Patel <JeelPatel@Jeels-MacPro.attlocal.net>
2022-03-18 10:33:27 -0500

    Mock-1 Problems

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,36 @@
 
+//Missing element in  from Sorted array
+// Time Complexity : O(Log N base 2)
+// Space Complexity : O(1)
+
+// Did this code successfully run on Leetcode : NA
+
+// Any problem you faced while coding this : No
+
+// Your code here along with comments explaining your approach
+
+public class Problem1 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,8,9};
+        
+        int l = 0, h = arr.length - 1;
+        
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            
+            //If we find that difference between element and its index is not equal for element at mid and low index
+            //We will chnage to value of mid according to that
+            if(arr[mid] - mid != (arr[l] - l)){
+                h = mid;
+            }
+            if(arr[h] - h != (arr[mid] - mid)){
+                l = mid;
+            }
+            
+            if(h-l == 1){ // Only 2 elements, find middle and it will be the missing elemeNT
+                break;
+            }
+        }
+        System.out.println("Missing element is : " + ( arr[l] + arr[h] ) / 2);
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,117 @@
 
+//Design Min_Heap - Parent is less than both of its children
+//First element in the Min-heap array will be the smellest element
+
+// Time Complexity : 
+/* For Insert and ExtractMin : O(Log N base 2)
+For getMin and getLength : O(1)
+
+*/
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+
+class MinHeap{
+    int[] heap;
+    int len;
+    public MinHeap(){
+        heap = new int[100]; //Heap of 100 elements
+        len = -1; //Curr length of heap;
+    }
+    public int getMin(){ // O(1)
+        return heap[0];
+    }
+    public int getSize(){ //O(1)
+        return len + 1;
+    }
+    public void insert(int n){ //O(Log N base 2)
+        if(len == -1){
+            len++;
+            heap[len] = n;
+            return;
+        }
+        len++;
+        heap[len] = n;
+        heapify(len); // After inserting an element in heap
+    }
+    public int extractMin(){ //O(Log N base 2)
+        int first = heap[0];
+        
+        swap(0,len);
+        minHeapify(0, len-1); // After extracing min element from heap 
+        len--;
+        return first;
+    }
+    public void swap(int i, int j){
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+    public void heapify(int i){
+        if(i == 0)  return;
+        
+        int parent =(int) Math.floor((i-1)/2);
+        
+        if(parent >= 0 && heap[i] < heap[parent]){
+            swap(i, parent);
+            heapify(parent); // Checking parent of element at curr Parent index
+        }
+    }
+    public void printMinHeap(){
+        for(int p = 0; p <= len; p++){
+            System.out.print(heap[p] + "  ");
+        }
+        System.out.println();
+    }
+    public void minHeapify(int i, int l){
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+        
+        int temp = i;
+        
+        if( left <= l && heap[left] < heap[temp] ){
+            temp = left;
+        }
+        if( right <= l && heap[right] < heap[temp] ){ //If above conditin true, Temp will be updated and  that valuw will be checked
+            temp = right;
+        }
+        
+        if(temp == i){
+            return;
+        }
+        swap(i, temp);
+        minHeapify(temp, l);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MinHeap m = new MinHeap();
+        m.insert(15); m.insert(28); m.insert(16);
+        m.insert(32); m.insert(31); m.insert(20);
+        
+        System.out.print("Current Heap : ");
+        m.printMinHeap();
+        
+        System.out.println("Now Inserting 5 in MinHeap");
+        m.insert(5); 
+        m.printMinHeap();
+
+
+        System.out.println("Now Inserting 10 in MinHeap");
+        m.insert(10);
+        
+        System.out.print("Current Heap : ");
+        m.printMinHeap();
+        
+        System.out.println("Extracting min from Heap " );
+        System.out.println("Extracted from MinHeap : " + m.extractMin());
+        
+        m.printMinHeap();
+        
+    }
+}
