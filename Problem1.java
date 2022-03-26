4b92cec1bb40722702de3672cdbf8ff684e7bd56
Shrutika Mokashi <mokashi.shrutika12345@gmail.com>
2022-03-25 21:07:13 -0400

    Completed Week 1 Competitive-Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,34 @@
 
+// "static void main" must be defined in a public class.
+// Time Complexity
+// With bruteforce solution: O(m*n)
+// With Binary search Optimization: O(Log m + Log n)
+// Where n = number of elements in array.
+// Space Complexity
+// With bruteforce solution: O(1)
+// With Binary search Optimization: O(1)
+
+import java.util.*;
+
+public class ClassMain {
+    public static int missingNumber(int nums[], int size) {
+        int low = 0;
+        int high = size - 1;
+        while (high - low >= 2) {
+            int mid = low + (high - low) / 2;
+            if ((nums[mid] - mid) != (nums[low] - low)) {
+                high = mid;
+            } else if ((nums[mid] - mid) != (nums[high] - high)) {
+                low = mid;
+            }
+        }
+        return (nums[high] + nums[low]) / 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int[] arr = { 2, 3, 4, 5, 6, 7, 8, 9 };
+        int size = arr.length;
+        System.out.println("Missing number: " + missingNumber(arr, size));
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,69 @@
 
+// "static void main" must be defined in a public class.
+import java.util.*;
+
+public class MinHeap {
+    private int size;
+    private List<Integer> minHeap;
+    private int maxSize;
+
+    public MinHeap(int maxSize) {
+        this.size = 0;
+        this.maxSize = maxSize;
+        this.minHeap = new ArrayList<>(maxSize);
+    }
+
+    public int getMin() {
+        return minHeap.get(0);
+    }
+
+    public boolean isEmpty() {
+        if (this.minHeap.size() == 0)
+            return true;
+        return false;
+    }
+
+    public void insert(int elem) {
+        if (isEmpty()) {
+            this.minHeap.add(elem);
+            size++;
+            return;
+        }
+        if (size == maxSize + 1) {
+            System.out.println("Heap is full!");
+            return;
+        }
+        this.minHeap.add(elem);
+        size++;
+        bubleUp(size - 1);
+    }
+
+    public void remove() {
+        int temp = minHeap.get(size);
+        minHeap.add(size, this.minHeap.get(0));
+        minHeap.add(0, temp);
+        minHeap.remove(size);
+        bubleDown(size - 1);
+    }
+
+    public void bubleUp(int index) {
+        if (size == 0)
+            return;
+        int parent = (index - 1) / 2;
+        if (parent >= 0 && (this.minHeap.get(index) < this.minHeap.get(parent))) {
+            int temp = this.minHeap.get(index);
+            minHeap.add(index, this.minHeap.get(parent));
+            minHeap.add(parent, temp);
+        }
+
+    }
+
+    public void bubleDown(int size) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        ArrayList<Integer> list = new ArrayList<>();
+    }
+}
