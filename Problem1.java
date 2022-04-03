f366d0c304e9822df8b94d15ba06d59e23962742
tanmay24497 <tanmayagrawal244@gmail.com>
2022-04-03 10:44:51 -0700

    Completed Missing Number

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,27 @@
+/*package whatever //do not write package name here */
 
+import java.io.*;
+
+class GFG {
+    public static int search(int[]nums,int size){
+        int l =0;
+        int h = size-1;
+        int mid;
+        while(l<h-1){
+            mid = l + (h-l)/2;
+            if(nums[mid]-nums[l]!=mid-l){
+                h = mid;
+            }
+            else if(nums[h]-nums[mid]!=h-mid){
+                l = mid;
+            }
+        }
+        return (nums[h]+nums[l])/2;
+    }
+	public static void main (String[] args) {
+		int ar[] = {  6, 8 };
+        int size = ar.length;
+        System.out.println("Missing number:" +
+                        search(ar, size));
+	}
+}
