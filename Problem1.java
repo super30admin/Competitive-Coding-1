b9f1c414451cf3d5954fc5aff1504488b9738c3e
unknown <krishnaa.gsk@gmail.com>
2022-04-06 08:54:31 -0700

    Completed Competitive coding-1 in java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,33 @@
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : not found in leetcode
+// Any problem you faced while coding this : no
 
+import java.io.*;
+
+class SearchMissingElement {
+    
+    public static int search(int[] nums, int size) {
+        int low = 0, high = size - 1;
+        
+        while(low <= high) {
+            
+            int mid = low + (high - low) / 2;
+            
+            if(high - low == 1 && nums[high] - nums[low] > 1) {
+                return nums[low] + 1;
+            } else if(mid - low == nums[mid] - nums[low]) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+
+        return -1;
+    }
+	public static void main (String[] args) {
+		int ar[] = {4, 5, 6, 8 };
+        int size = ar.length;
+        System.out.println("Missing number: " + search(ar, size));
+	}
+}
