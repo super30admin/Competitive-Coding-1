e52f3d9f895dfd12bcc9b44883f916c2ebe90f0f
Rincy Soman <rincysoman@gmail.com>
2022-05-03 11:05:26 -0700

    Adding time and space complexity

--- FindMissingNumber.cs
+++ FindMissingNumber.cs
@@ -4,6 +4,10 @@ namespace Algorithms
     public class FindMissingNumber
     {
 
+        /// Time Complexity : O(LogN) 
+        // Space Complexity :O(1)
+        // Did this code successfully run on Leetcode :Yes
+        // Any problem you faced while coding this :  No
         public int SearchMissingNumber(int[] nums)
         {
             if (nums.Length == 0) return -1;
@@ -25,8 +29,6 @@ namespace Algorithms
                     {
                         low = mid;
                     }
-
-
             }
             return (nums[high] + nums[low])/2;
 
