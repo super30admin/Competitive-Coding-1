abc9fcf107afda890f2a6e2f645fbfb753db4563
Rincy Soman <rincysoman@gmail.com>
2022-05-02 18:00:39 -0700

    Find Missing number in an array

---
+++ FindMissingNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Algorithms
+{
+    public class FindMissingNumber
+    {
+
+        public int SearchMissingNumber(int[] nums)
+        {
+            if (nums.Length == 0) return -1;
+            int low = 0, high = nums.Length - 1;
+            int mid = 0;
+
+            while (high- low >=2)
+            {
+                mid = low + (high - low) / 2;
+
+                int midIndexDiff = nums[mid] - mid;
+                int lowIndexDiff = nums[low] - low;
+
+                    if (midIndexDiff != lowIndexDiff)
+                    {
+                        high = mid;
+                    }
+                    else
+                    {
+                        low = mid;
+                    }
+
+
+            }
+            return (nums[high] + nums[low])/2;
+
+        }
+    }
+}
