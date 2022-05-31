edc0a523bf233ed90e52ab6cc612f93ce48deab1
Drusti Thakkar <tdrusti@vmware.com>
2022-05-31 13:03:59 -0700

    Competitive-Coding 1 sprint complete. Please review

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,35 @@
+// Provide an array of n-1 integers in the range of 1 to n with no duplicates in list, One of the integers is missing in the list. 
+// Write a code to find the missing integer.
 
+// Time Complexity: O(n) - Traversing all numbers in the array + 1
+// Space Complexity: O(n) - Holding an array of input array length to store data
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class MissingInteger {
+
+    int getMissingInteger(int[] n) {
+        // Creating a boolean array of size n.length+1 to store which numbers in range 1 to n exists
+        boolean[] temp = new boolean[n.length+1];
+        for(int i=0; i<n.length; i++) {
+            temp[n[i]-1] = true;
+        }
+
+        // Checking the boolean array to find missing number 
+        for(int i=0; i<temp.length; i++) {
+            if(temp[i] == false) {
+                return i+1;
+            }
+        }
+
+        // Return the last number + 1 if all numbers are in sequence in the given array.
+        return n.length+1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8};
+        MissingInteger m = new MissingInteger();
+        int missingNumber = m.getMissingInteger(arr);
+        System.out.println("Missing number in array is " + missingNumber);
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,114 @@
+import java.util.Arrays;
+import java.util.NoSuchElementException;
 
+// Time Complexity: O(1) - getMin(), O(logn) - extractMin(), O(logn) - insert()
+// Space Complexity: O(1) - No extra dataset used
+// Did this code successfully run on Leetcode : No
+// Any problem you faced while coding this : No
+
+class MinHeap {
+
+    private int[] heap;
+    private int size;
+    private int maxSize;
+
+    public MinHeap(int maxSize) {
+        this.maxSize = maxSize;
+        this.size = 0;
+
+        heap = new int[maxSize];
+    }
+
+    public int getMin() {
+        if(heap.length == 0) {
+            throw new NoSuchElementException();
+        }
+        return heap[0];
+    }
+
+    public int extractMin() {
+        if(heap.length == 0) {
+            throw new NoSuchElementException();
+        }
+
+        int minElement = heap[0];
+        heap[0] = heap[size-1];
+        size--;
+        heapifyDown();
+        return minElement;
+    }
+
+    public void insert(int item) {
+        ensureCapacity();
+        heap[size] = item;
+        size++;
+        heapifyUp();
+    }
+
+    public void heapifyUp() {
+        int index = size-1;
+        while(hasParent(index) && parent(index) > heap[index]) {
+            swap(getParentIndex(index), index);
+            index = getParentIndex(index);
+        }
+    }
+
+    public void heapifyDown(){
+        int index = 0;
+        while(hasLeftChild(index)) {
+            int smallerChildIndex = getLeftChildIndex(index);
+            if(hasRightChild(index) && rightChild(index) < leftChild(index)) {
+                smallerChildIndex = getRightChildIndex(index);
+            }
+
+            if(heap[index] < heap[smallerChildIndex]) {
+                break;
+            } else {
+                swap(index, smallerChildIndex);
+                
+            }
+
+            index = smallerChildIndex;
+
+        }
+    }
+
+    // Helper methods to find correct item based on left and right child indexes
+    private int getLeftChildIndex(int parentIndex) { return (2*parentIndex)+1; }
+    private int getRightChildIndex(int parentIndex) { return (2*parentIndex)+2; }
+    private int getParentIndex(int childIndex) { return (childIndex-1)/2; }
+
+    private int leftChild(int index) { return heap[getLeftChildIndex(index)]; }
+    private int rightChild(int index) { return heap[getRightChildIndex(index)]; }
+    private int parent(int index) { return heap[getParentIndex(index)]; }
+
+    private boolean hasLeftChild(int index) { return leftChild(index) < size; }
+    private boolean hasRightChild(int index) { return rightChild(index) < size; }
+    private boolean hasParent(int index) { return parent(index) >= 0; }
+
+    // Swap elements at 2 indices to bubble up or down the minimum/new element
+    private void swap(int indexOne, int indexTwo){
+        int temp = heap[indexOne];
+        heap[indexOne] = heap[indexTwo];
+        heap[indexTwo] = temp;
+    }
+
+    private void ensureCapacity(){
+        if(size == maxSize) {
+            heap = Arrays.copyOf(heap, maxSize*2);
+            maxSize = maxSize*2;
+        }
+    }
+
+    public static void main(String[] args) {
+        MinHeap heap = new MinHeap(10);
+        heap.insert(10);
+        heap.insert(4);
+        heap.insert(2);
+        heap.insert(1);
+        heap.insert(5);
+        System.out.println(heap.getMin());
+        heap.extractMin();
+        System.out.println(heap.getMin());
+    }
+}
