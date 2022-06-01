d2817b89c9ababb724425849dc6f69258b14fb67
ishankaggarwal <ishank193aggarwal@gmail.com>
2022-06-02 00:18:04 +0530

    "Completed Competitive-Coding-1"

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,1 +1,28 @@
+// Provide an array of n-1 integers in the range of 1 to n with no duplicates in list, One of the integers is missing in the list. Write a code to find the missing integer.
 
+// Examples:
+// Input : arr[] = [1, 2, 3, 5, 6, 7, 8]
+// Output : 4
+
+//Time Complexity- O(logn)
+//Space Complexity- O(1)
+
+int search(int ar[], int size)
+{
+	// Extreme cases
+	if (ar[0] != 1)
+		return 1;
+	if (ar[size - 1] != (size + 1))
+		return size + 1;
+
+	int a = 0, b = size - 1;
+	int mid;
+	while ((b - a) > 1) {
+		mid = (a + b) / 2;
+		if ((ar[a] - a) != (ar[mid] - mid))                 //Elements to the left of missing element will have this property: ar[i]-i=1
+			b = mid;
+		else if ((ar[b] - b) != (ar[mid] - mid))            //Elements to the right of missing element will have this property: ar[i]-i=2
+			a = mid;
+	}
+	return (ar[a] + 1);
+}
--- Problem2.cpp
+++ Problem2.cpp
@@ -1,1 +1,103 @@
+//Min Heap in C++
 
+#include<bits/stdc++.h>
+using namespace std;
+
+// A class for Min Heap
+class MinHeap{
+    int *heap_array; // pointer to array of elements in heap
+    int capacity; // maximum possible size of min heap
+    int heap_size; // Current number of elements in min heap
+
+public:
+    // Constructor: Initialise a capacity and heap_array;
+    MinHeap(int capacity){
+        this->heap_size = 0;
+        this->capacity = capacity;
+        this->heap_array = new int[capacity];
+    }
+
+    // method to heapify a subtree with the root at given index i
+    //Time Complexity- O(logn)
+    void MinHeapify(int i){
+        /* A recursive method to heapify 'heap_array' */
+        int l = left(i);
+        int r = right(i);
+
+        int smallest = i;
+        if (l < heap_size && heap_array[l] < heap_array[i])
+            smallest = l;
+        if (r < heap_size && heap_array[r] < heap_array[smallest])
+            smallest = r;
+
+        if (smallest != i){
+            swap(heap_array[i], heap_array[smallest]);
+            MinHeapify(smallest);
+        }
+    }
+
+    // method to get index of parent of node at index i
+    int parent(int i){ return (i-1)/2; }
+
+    // method to get index of left child of node at index i
+    int left(int i){ return (2*i + 1); }
+
+    // method to get index of right child of node at index i
+    int right(int i){ return (2*i + 2); }
+
+    // method to remove minimum element (or root) from min heap
+    //Time Complexity- O(logn)
+    int extractMin(){
+        if (heap_size <= 0)
+            return INT_MAX;
+        if (heap_size == 1){
+            heap_size--;
+            return heap_array[0];
+        }
+
+        // remove the minimum value from the heap.
+        int root = heap_array[0];
+        heap_array[0] = heap_array[heap_size-1];
+        heap_size--;
+        MinHeapify(0);
+
+        return root;
+    }
+
+    // Returns the minimum key (key at root) from min heap
+    //Time Complexity- O(1)
+    int getMin(){ return heap_array[0]; }
+
+    // method to inserts a new key 'k'
+    //Time Complexity- O(logn)
+    void insert(int k){
+        if (heap_size == capacity){
+            cout << "\nOverflow: Could not insertKey\n";
+            return;
+        }
+
+        // Inserting the new key at the end
+        int i = heap_size;
+        heap_array[heap_size++] = k;
+
+        while (i != 0 && heap_array[parent(i)] > heap_array[i]){
+            swap(heap_array[i], heap_array[parent(i)]);
+            i = parent(i);
+        }
+    }
+};
+
+// Driver program to test above functions
+int main(){
+    MinHeap h(11);
+    h.insertKey(15);
+    h.insertKey(2);
+    h.insertKey(3);
+    h.insertKey(1);
+    h.insertKey(45);
+    h.insertKey(5);
+    h.insertKey(4);
+    cout << "minimum element: " << h.extractMin() << endl;
+    cout << "second minimum element: " << h.getMin() << endl;
+    return 0;
+}
