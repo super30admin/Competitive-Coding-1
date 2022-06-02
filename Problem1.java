952505368d5ae14fbb1b820b3f6ebdd73029b8d5
NishanthSayana <snvnishanth99@gmail.com>
2022-06-02 15:46:20 +0530

    Compitetve Programming-1 Please review

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,22 @@
+public Problem1{
+public static int serachNumber(int arr[]){
+        int l=0,h=arr.length-1;
 
+        while(h-l>=2){
+            int m=l+(h-l)/2;
+            if(arr[m]-m!=arr[l]-l){
+                h=m;
+            }else if(arr[m]-m!=arr[h]-h){
+                l=m;
+            }
+        }
+        return (arr[l]+arr[h])/2;
+    }
+
+public static void main(String[] args){
+
+        int arr []={1,2,3,4,5,6,8};
+
+        System.out.println(serachNumber(arr));
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,80 @@
+public class MinHeap {
+
+    private int heap[];
+    private int size;
+    private int maxSize;
+    private static int front=1;
+
+    public MinHeap(int maxSize) {
+        this.maxSize = maxSize;
+        this.size=0;
+        heap=new int[maxSize+1];
+    }
+
+    public int parent(int pos){
+        return pos/2;
+    }
+    public int leftChild(int pos){
+        return 2*pos;
+    }
+
+    public int rightChild(int pos){
+        return 2*pos+1;
+    }
+
+    public boolean isLeaf(int pos)
+    {
+        if(pos>size/2 && pos<=size){
+            return true;
+        }
+        return false;
+    }
+
+    public void swap(int first, int second){
+        int temp=heap[first];
+        heap[first]=heap[second];
+        heap[second]=temp;
+    }
+    public void minHeapify(int pos){
+
+        if(!isLeaf(pos)){
+            if(heap[pos]>heap[leftChild(pos)]){
+                swap(pos,leftChild(pos));
+                minHeapify(leftChild(pos));
+            }else if(heap[pos]>heap[rightChild(pos)]){
+                swap(pos,rightChild(pos));
+                minHeapify(rightChild(pos));
+            }
+        }
+    }
+
+    public void insert(int element){
+        if(size>=heap.length){
+            return;
+        }
+
+        heap[++size]=element;
+        int current=size;
+        while(heap[current]<heap[parent(current)]){
+            swap(current,parent(current));
+            current=parent(current);
+        }
+
+    }
+    public int remove(){
+        int remove=heap[front];
+        heap[front]=heap[size--];
+        minHeapify(front);
+        return remove;
+    }
+
+    public void print() {
+        for (int i = 1; i <= size / 2; i++) {
+
+            System.out.print(" Parent : " + heap[i] + " Left Child : " + heap[2 * i] + " Right Child :" + heap[2 * i + 1]);
+            System.out.println();
+        }
+    }
+
+}
 
