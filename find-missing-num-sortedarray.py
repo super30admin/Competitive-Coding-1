e3b95ff80460f8bf2a08646bab5fa4f462cc3619
Dixika Grewal <59345244+dixg@users.noreply.github.com>
2022-06-03 01:47:29 -0700

    q1 added details

--- find-missing-num-sortedarray.py
+++ find-missing-num-sortedarray.py
@@ -1,3 +1,7 @@
+# Time Complexity : log(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this : No
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         nums.sort()    
