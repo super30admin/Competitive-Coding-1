12e90d59bcd5e599f41d82880abaaae03b2ecc29
STSC <STSC@LENOVO-127795.ad.csueastbay.edu>
2022-06-03 16:42:24 -0700

    Completed the Missing element

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,30 @@
+/* Time Complexity: O(log(N))
+ * Space Complexity:o(1)
+ * Any problem you faced while coding this : No
+ */
 
+class MissingElement {
+    static int search(int nums[], int n)
+    {
+    	int low =0;
+    	int high = n - 1;
+    	
+    	while(high - low >= 2) {
+    		int mid  = low + (high - low)/2;
+    		if((arr[low] - low) != (arr[mid] - mid)) {
+    			high = mid;
+    		} else {
+    			low = mid;
+    		}
+    	}
+    	
+    	return (arr[low] + 1);
+    }
+
+    public static void main(String[] args)
+    {
+        int nums[] = { 1, 2, 3, 4, 6 };
+        int n = nums.length;
+        System.out.println(search(nums, n));
+    }
+}
