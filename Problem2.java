b6a36163700b7abfc210b58b927fcb7226a10cfb
AishwaryaBabu2 <107521970+AishwaryaBabu2@users.noreply.github.com>
2022-06-27 23:19:56 -0700

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,33 @@
 
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+//205 Leetcode
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        HashMap<Character,Character> sMap= new HashMap();
+        HashMap<Character,Character> tMap= new HashMap();
+
+        for(int i=0;i<s.length();i++){
+            char sChar=s.charAt(i);
+            char tChar=t.charAt(i);
+            if(sMap.containsKey(sChar)){
+                if(sMap.get(sChar) != tChar){
+                    return false;
+                }
+            }
+            else{
+                sMap.put(sChar, tChar);
+            }
+            if(tMap.containsKey(tChar)){
+                if(tMap.get(tChar) != sChar){
+                    return false;
+                }
+            }
+            else{
+                tMap.put(tChar, sChar);
+            }
+        }
+        return true;
+    }
+}
