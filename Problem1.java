f9cf4c6fdaf8b70ae8f3cda9f7a97d599aaadb8d
AishwaryaBabu2 <107521970+AishwaryaBabu2@users.noreply.github.com>
2022-06-27 23:15:45 -0700

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,47 @@
 
+
+class Solution 
+{
+    public int search(ArrayReader reader, int target) 
+    {
+        int low=0;
+        int high=1;
+    
+        if(reader.get(0)==Integer.MAX_VALUE)
+        {
+            return -1;
+        }
+        
+        while(target > reader.get(high))
+        {   
+            low=high;
+            high=high*2;
+             if(reader.get(high)== target)
+            {
+                return high;
+            }
+            
+        }
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            
+            if(reader.get(mid) == target)
+            {
+                return mid;
+            }
+            else if(reader.get(mid) > target)
+                    
+                    {
+                        
+                        high = mid-1;
+                    }
+                    else
+                    {
+                        low= mid+1;
+                    }
+        }
+                    
+               return -1;     
+      }
+}
