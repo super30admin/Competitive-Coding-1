38542b04bfd0f68797e3ad97839e61efa48b2aa2
Tushar <tusharpawar611>
2022-06-29 00:24:17 -0700

    completed Leetcode problem no 278

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,28 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
 
+      public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            
+            int low = 1;
+            int high = n;
+            
+    
+            
+            while(low<high){
+                int mid = low+(high-low)/2;           
+        
+                if(!isBadVersion(mid)){
+                    low= mid+1;
+                }
+                else{
+                    high = mid;
+                }
+                
+    
+            }
+            
+            
+            return low;
+        }
+    }
