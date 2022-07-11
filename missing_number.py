d13fb23e5c757afc719df0fcb57292861e29602d
navya0405 <navya.spin@gmail.com>
2022-07-10 18:11:15 -0700

    update complexities and approach

--- missing_number.py
+++ missing_number.py
@@ -1,3 +1,7 @@
+#time complexity: O(log n)
+# space complexity: O(1)
+# approach: check if we have crossed the missing element or not and update low and high accordingly
+
 arr = [1, 2, 3, 5, 6, 7, 8]
 n = len(arr)
 if arr[0]!= 1:
