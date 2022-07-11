1d9adf14c3db822637cf346baa289100ac8e9ffd
navya0405 <navya.spin@gmail.com>
2022-07-10 18:09:07 -0700

    missing number completed

---
+++ missing_number.py
@@ -0,0 +1,20 @@
+arr = [1, 2, 3, 5, 6, 7, 8]
+n = len(arr)
+if arr[0]!= 1:
+    print(1)
+elif arr[n-1] != n+1:
+    print(n+1)
+else:
+    low = 0
+    high = len(arr)
+    while(low+1 != high):
+        mid = low + (high - low)//2
+        if arr[low]-low != arr[mid]-mid:
+            high = mid
+        elif arr[high]-high != arr[mid]-mid:
+            low = mid
+    print(arr[low]+1)
+        
+
+        
+
