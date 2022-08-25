c204d1de277e43339edea41a0fc9e59796f85cc3
suraj7696 <satmakur@asu.edu>
2022-08-24 20:02:45 -0400

    resolved errors

--- interviewProblem.py
+++ interviewProblem.py
@@ -1,11 +1,11 @@
 def findMissingNumber(self, nums:List[int]) -> int:
-    if nums[0] !== 1:
+    if nums[0] != 1:
         return 1
     low, high = 0, len(nums)-1
     while low <= high:
         mid = (low+high)//2
-        if nums[low]-low !== nums[mid]-mid:
+        if nums[low]-low != nums[mid]-mid:
             high = mid
         else:
             low = mid + 1
-    return mid + 1
+    return low+1
