75c2a22465b55135323a8646de311695d97cb13f
BhargavVasudevaVunnam <vvbhargavchowdary@gmail.com>
2022-08-30 19:09:45 -0500

    Completed Sprint Competitive Coding-1 Added Min heap

---
+++ minheap.java
@@ -0,0 +1,23 @@
+/*Time complexity:O(n log k)
+Space Complexity: k
+*/
+import java.util.*;
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+    for(int i=0;i<k;i++)
+    {
+        pq.add(nums[i]);
+    }
+    
+    for(int i=k;i<nums.length;i++)
+    {
+        if(nums[i]>pq.peek())
+        {
+            pq.poll();
+            pq.add(nums[i]);
+        }
+    }
+        return pq.peek();
+}
+}
