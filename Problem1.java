1385971228fc6f7e80b934d772372e6406f01c5a
Prerana Rajeshvar Kharat <preranarajeshvarkharat@Preranas-MacBook-Pro.local>
2022-09-14 09:09:32 -0700

    Completed Competitive-Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums==null || nums.length==0){
+            return -1;
+        }
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid= low+(high-low)/2;
+            if((mid==high||nums[mid]>nums[mid+1]) && (mid==low||nums[mid]>nums[mid-1])){
+                return mid;
+            }
+            else if(nums[mid]<nums[mid+1]){
+                low= mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
