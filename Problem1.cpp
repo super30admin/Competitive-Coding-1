51dac45f1ec8d8c07783fc5fc5e13c96852927ae
Priyank Bardolia <bardoliapriyank@gmail.com>
2022-09-13 17:46:36 -0700

    Completed

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,1 +1,16 @@
-
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        hashmap = {}
+        
+        if len(s) != len(t) or len(set(s)) != len(set(t)):
+            return False
+        
+        for i in range(len(s)):
+            if s[i] not in hashmap:
+                hashmap[s[i]] = t[i]
+            elif hashmap[s[i]] != t[i]:
+                return False
+        return True
+    
+    # Time Complexity: O(n)
+    # Space Complexity: O(n) because we used hashmap
