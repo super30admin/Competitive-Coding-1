9008ea7ee61ebabd57b1226c093a1b117ea4ccc9
Arbind Singh <getarbind@gmail.com>
2022-12-27 19:18:26 -0500

    commit1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,25 @@
+// find missing number in a sorted array
+public class Problem1{
 
+    public static void main(String[] args){
+        int[] num = {1,3,4,5,6,7,8,9};
+        int missingNum = findMissingNum(num);
+        System.out.println(missingNum);
+
+    }
+
+    static int findMissingNum(int[] num){
+        int left = 0;
+        int right = num.length-1;
+        int mid = 0;
+        while (left < right){
+            mid = left + (right-left)/2;
+            if (num[mid-1]> mid){
+                right = mid;
+            } else {
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+}
