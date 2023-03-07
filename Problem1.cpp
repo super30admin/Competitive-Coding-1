0ceded41b3c00af41533c7c9cbf7b1cbb28c98a9
Manasa Nannuru <mnannuru@albanu.edu>
2023-03-06 22:33:56 -0500

    Done with find missing number in a sorted array

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,1 +1,18 @@
-
+int search(vector<int> arr)
+{
+    // Time Complexity - O(n)
+    // Space Complexity - S(1)
+    int size = arr.size();
+    if (ar[0] != 1)
+        return 1;
+    if (ar[size - 1] != (size + 1))
+        return size + 1;
+ 
+    int low = 0, high = size - 1;
+    int mid;
+    while(low<high){
+        if(arr[mid] == mid +1) low = mid+1;
+        else high = mid;
+    }
+    return (arr[high] + 1);
+}
