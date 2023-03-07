5dcb0d2e34418c9e2d46b5677187f4958e38629a
Devyash Sanghai <devyashsanghai@gmail.com>
2023-03-06 22:45:16 -0800

    problem 1 done

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,23 @@
+class MinHeap{
+    clas
 
+    int getMin(){
+        return -1;
+    }
+    int extractMin(){
+        return -1;
+    }
+    void insert(int x){
+    }
+}
+
+class Node{
+    Node left;
+    Node right;
+    int val;
+    Node(Node left, Node right, int val){
+        this.left = left;
+        this.right = right;
+        this.val = val;
+    }
+}
