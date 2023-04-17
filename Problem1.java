427532c887e858f9d6f28383786abcef58e64e99
Pavan Rao <pavanpej@gmail.com>
2023-04-17 17:53:13 +0530

    Competitive-Coding-1 completed; please review

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,38 @@
+// Question - Find Missing Number in a sorted array
+// Time complexity - O(log(n))
+// Space complexity - O(1)
 
+public class Problem1 {
+    public static int findMissing(int[] a) {
+        if (a == null || a.length == 0) {
+            return -1;
+        }
+
+        int low = 0, high = a.length - 1;
+
+        int mid = -1;
+
+        if (high == a[high] - 1) {
+            return -1;
+        }
+
+        while (low <= high) {
+            mid = low + (high - low) / 2; // prevent overflow
+
+            if (a[mid] == mid + 1) { // left is sorted
+                low = mid + 1;
+            } else { // right is sorted
+                high = mid - 1;
+            }
+        }
+
+        return mid + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] tc = { 1, 2, 3, 4, 5, 6 };
+        // int[] tc = {1};
+        int missing = findMissing(tc);
+        System.out.println(missing);
+    }
+}
