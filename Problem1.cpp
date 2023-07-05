9a5f89e3bee3e5eabb7df3d61b59e4f395d503b4
Priyanshu Shrivastava <109136206+Spriy4nshu@users.noreply.github.com>
2023-07-05 19:31:09 -0400

    Competitive-Coding-1

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,1 +1,37 @@
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
 
+#include <iostream>
+#include <vector>
+  
+using namespace std;
+//use binary search and index to locate the missing element
+class Solution {
+public:
+    int findsMissing(vector<int> arr) {
+        int l = 0;
+        int h = arr.size() - 1;
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            if (arr.empty())
+                return -1;
+            else if (arr[h] == h + 2)
+                return h + 1;
+            else if (arr[m] == m + 2)
+                return m + 1;
+            else if (arr[l] - l == arr[m] - m)
+                l = m + 1;
+            else
+                h = m - 1;
+        }
+        return -1;
+    }
+};
+int main() {
+    Solution solution;
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 9};
+    int missing = solution.findsMissing(arr);
+    cout << "Missing number: " << missing << endl;
+    return 0;
+}
--- Problem2.cpp
+++ Problem2.cpp
@@ -1,1 +1,49 @@
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
 
+#include <iostream>
+#include <vector>
+  
+using namespace std;
+
+
+//use vector to implement minheap functionality
+class MinHeap {//O(logn)
+  std::vector<int> data;
+
+  void push(int value) {
+    data.push_back(value);
+    int current = data.size() - 1;
+    while (current > 0 && data[current] < data[(current - 1) / 2]) {
+      std::swap(data[current], data[(current - 1) / 2]);
+      current = (current - 1) / 2;
+    }
+  }
+
+  int pop() {//O(logn) height of the tree
+    int value = data[0];
+    data[0] = data.back();
+    data.pop_back();
+    int current = 0;
+    while (current < data.size() - 1) {
+      int leftChild = 2 * current + 1;
+      int rightChild = 2 * current + 2;
+      int smallerChild = leftChild;
+      if (rightChild < data.size() && data[rightChild] < data[leftChild]) {
+        smallerChild = rightChild;
+      }
+      if (data[current] > data[smallerChild]) {
+        std::swap(data[current], data[smallerChild]);
+        current = smallerChild;
+      } else {
+        break;
+      }
+    }
+    return value;
+  }
+
+  bool empty() {//O(1)
+    return data.empty();
+  }
+};
