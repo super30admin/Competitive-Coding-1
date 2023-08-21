0932d1e2202d398fa6edc72baf059ba3cad8e5d5
Sai Vinherkar <sai.vinherkar@angi.com>
2023-08-20 21:33:22 -0500

    Corrected code.

--- MissingElement.java
+++ MissingElement.java
@@ -3,13 +3,14 @@
 
 class MissingElement{
    public int findMissingNumber(int arr[]){
+    int arr[] = new int[] {1,2,3,4,6,7};
         int n = arr.length;
         int out = 0;
         for(int i=0; i<arr.length; i++){
-            if(arr[i]-i >1){
-                out = nums[i-1];
+            if(arr[i]-i > 1){
+                out = i;
             }
         }
-        return out;
+       System.out.print(out);
     }
 }
