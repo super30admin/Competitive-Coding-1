958fef517a9fda717246e7aa67a3c4a6922ea3a1
deeksha <9107dt@gmail.com>
2023-09-06 15:57:33 -0400

    Design-2 Complete

---
+++ Problem2.py
@@ -0,0 +1,20 @@
+# Min Heap Implementation using Array
+# # // Time Complexity :
+# # // Space Complexity :
+# # // Did this code successfully run on Leetcode :
+# # // Any problem you faced while coding this :
+
+
+# # // Your code here along with comments explaining your approach
+
+# Step 1: Read Question
+# Step 2: Constraints
+# Step 3: Test Cases
+#         [0, 1, 3, 4, 6, 7] --> 2
+#         [0, 1, 2, 3, 4, 5, 6, 7, 9] --> 8
+#         [0, 2, 3, 4, 5, 6, 7] --> 8
+# Step 4: Solution without Code
+#         Brute Force: Linear Search
+#         Optimised: Binary Search --> finding the difference between the index and the num[i]
+#         else go where missing high = mid - 1
+# Step 5: Code the solution
