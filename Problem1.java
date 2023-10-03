8c7db519bec1586507f37750c72d7eb7008fa0d7
Sharathpatangi <sharathpatangi@gmail.com>
2023-10-03 14:56:12 -0700

    Completed Competitive-Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,22 @@
+class FindMissingNumberSortedArray{
+    public static void main(String args[]){
+        int[] arr = new int[]{1,2,3,4,6,7};
+        int low = 0, high = arr.length-1;
 
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if((arr[mid] - mid) > 1 && (arr[mid] - arr[mid-1]) !=1){
+                System.out.println(arr[mid]-1);
+                system.exit(0);
+            }
+            else if(arr[mid] - mid == 1){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        System.out.println("-1");
+    }
+}
