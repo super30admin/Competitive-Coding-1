1f391813534b8df507e5d53f315bc1ffa4aefd9c
Yatish Koul <36380820+yatishkoul@users.noreply.github.com>
2023-10-06 16:56:08 -0700

    Update Missing_number_in_array.cpp

--- Missing_number_in_array.cpp
+++ Missing_number_in_array.cpp
@@ -1,19 +1,30 @@
 
-class Solution {
-public:
-    int findPeakElement(vector<int>& nums) {
-            int n = nums.size();
-        int low =0;
-        int high = n-1;
+class Solution{
+  public:
+    int missingNumber(vector<int>& a, int n) {
+        // Your code goes here
+        sort(a.begin(), a.end());
+        int low=0;
+        int high=a.size()-1;
+        if(a[0]!=1)return 1;
+        if(a[a.size()-1]!=n)return n;
         while(low<=high){
             int mid = low + (high-low)/2;
-            //if mid is the peak element
-            if((mid==0 || nums[mid]>nums[mid-1]) && (mid==n-1 || nums[mid]> nums[mid+1]))return mid;
-            else if(mid==n-1 || nums[mid]<nums[mid+1])low = mid+1;
-        
-        else high = mid -1;
+            if(a[mid]==mid+2){
+                if(mid>0 && a[mid]-a[mid-1]==2){
+                    return a[mid]-1; 
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+            
+            else {
+                if(mid<a.size())
+                low = mid+1;
+            }
         }
-    return -1;
-    }
     
+        return -1;
+    }
 };
