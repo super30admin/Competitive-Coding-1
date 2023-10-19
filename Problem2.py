97b323e52a39aaea5512ce59b346c075cacf34a4
deeksha <9107dt@gmail.com>
2023-10-18 23:02:50 -0400

    Competitive-Coding-1 Complete

--- Problem2.py
+++ Problem2.py
@@ -1,20 +1,62 @@
 # Min Heap Implementation using Array
-# # // Time Complexity :
-# # // Space Complexity :
-# # // Did this code successfully run on Leetcode :
-# # // Any problem you faced while coding this :
+# # // Time Complexity : push O(1)
+# # // Space Complexity : O()
+# # // Did this code successfully run on Leetcode : No
+# # // Any problem you faced while coding this :No
 
 
 # # // Your code here along with comments explaining your approach
+class heapq:
+    def __init__(self) -> None:
+        self.heap = []
+        print(self.heap)
 
-# Step 1: Read Question
-# Step 2: Constraints
-# Step 3: Test Cases
-#         [0, 1, 3, 4, 6, 7] --> 2
-#         [0, 1, 2, 3, 4, 5, 6, 7, 9] --> 8
-#         [0, 2, 3, 4, 5, 6, 7] --> 8
-# Step 4: Solution without Code
-#         Brute Force: Linear Search
-#         Optimised: Binary Search --> finding the difference between the index and the num[i]
-#         else go where missing high = mid - 1
-# Step 5: Code the solution
+    def push(self, element):
+        self.heap.append(element)
+        self.shiftUp(element)
+
+    def shiftUp(self, element):
+        pos = len(self.heap) - 1
+        while pos > 0:
+            parent = (pos - 1) >> 1
+            if self.heap[parent] > element:
+                self.heap[pos] = self.heap[parent]
+            else:
+                break
+            pos = parent
+        self.heap[pos] = element
+
+    def shiftdown(self, item):
+        pos = 0
+        child = pos*2 + 1
+        while child < len(self.heap):
+            right = child + 1
+            if right < len(self.heap) and not self.heap[child] < self.heap[right]:
+                child = right
+            if self.heap[child] < self.heap[pos]:
+                self.heap[pos], self.heap[child] = self.heap[child], self.heap[pos]
+            pos = child
+            child = pos*2 + 1
+
+    # always pop the top one
+    def pop(self):
+        pop_item = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.shiftdown(self.heap[0])
+        return pop_item
+
+
+# Binary Tree Representation
+# of input array
+# 1
+# / \
+# 3	 5
+# / \	 / \
+# 4	 6 13 10
+# / \ / \
+# 9 8 15 17
+heap = heapq()
+arr = [1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17]
+for ele in arr:
+    heap.push(ele)
+print(heap)
