df727794716bce3162818273a859ec02d311ba7e
Keerthana Vijaykumar <kevimach011997@gmail.com>
2023-12-25 19:59:36 -0700

    Completed completitive coding-1

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,1 +1,36 @@
+// Time Complexity : O(log n)
+ 
+// Space Complexity : O(1)
 
+// Did this code successfully run on Leetcode : YES
+
+// Appoarch: Sorted the numbers and started comparison with 
+// the indices. If the index and array value are same then
+// the search space is towards right. Once high and low becomes 
+// equal return the ans
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int ans = n;
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = (high + low)/2;
+            if(nums[mid] == mid){
+                low = mid+1;
+            }
+            else{
+                ans = mid;
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+};
--- Problem2.cpp
+++ Problem2.cpp
@@ -1,1 +1,100 @@
 
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class MinHeap
+{
+    vector<int> heap;
+    int parent(int i) { return (i - 1) / 2; }      // O(1)
+    int leftChild(int i) { return (2 * i + 1); }   // O(1)
+    int rightChild(int i) { return (2 * i + 2); }  // O(1)
+
+    void heapify(int i) //O(log n)
+    {
+        int l = leftChild(i);
+        int r = rightChild(i);
+        int smallest = i;
+        // check if the left is leafnode
+        if (l < heap.size() && heap[l] < heap[i])
+        {
+            smallest = l;
+        }
+        if (r < heap.size() && heap[r] < heap[smallest])
+        {
+            smallest = r;
+        }
+        if (smallest != i)
+        {
+            swap(heap[i], heap[smallest]);
+            heapify(smallest);
+        }
+    }
+
+public:
+    int getMin() // O(1)
+    {
+        if (heap.size() == 0)
+            return -1;
+        return heap.front();
+    }
+
+    void insert(int k) //O(log n)
+    {
+        heap.push_back(k);
+        int i = heap.size() - 1;
+        while (i != 0 && heap[parent(i) > heap[i]]) 
+        {
+            swap(heap[i], heap[parent(i)]);
+            i = parent(i);
+        }
+        heapify(0);
+    }
+
+    void deleteMin()  // O(log n) - worst case
+    {
+        if (heap.size() == 0)
+        {
+            cout << "Heap is empty" << endl;
+            return;
+        }
+        heap[0] = heap.back();
+        heap.pop_back();
+        heapify(0);
+    }
+
+    int size()
+    {
+        return heap.size();
+    }
+
+    void printHeap()  
+    {
+        for (int i = 0; i < heap.size(); ++i)
+        {
+            cout << heap[i] << "\t";
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    MinHeap H;
+    H.insert(20);
+    H.insert(8);
+    H.insert(5);
+    H.insert(10);
+    H.insert(15);
+    H.insert(12);
+
+    // Print all elements of the heap
+    H.printHeap();
+
+    cout << "The Min:" << H.getMin() << endl;
+
+    H.deleteMin();
+    H.printHeap();
+
+    return 0;
+}
