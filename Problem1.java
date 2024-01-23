42d3d71729be270065696b1fbc231dbcfce51186
Kripali Bhatodra <kripalibhatodra2206@gmail.com>
2024-01-23 12:05:44 -0800

    Competitive-Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,15 @@
+// TC- O(n)
+// SC- O(1)
 
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        int n=nums.length;
+        int sumN= n*(n+1)/2;
+        for(int num:nums){
+            sum=sum+num;
+        }
+        int number= sumN-sum;
+        return number;
+    }
+}
