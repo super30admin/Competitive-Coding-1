81f637d956995dba8c0a9de54cce3ffae150faf2
amolghar95 <129135362+amolghar95@users.noreply.github.com>
2024-01-28 14:10:39 -0500

    Interview Problem 1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,25 @@
-
+public class MyClass {
+    public static void main(String args[]) {
+      int[] nums = {2,3,4,5,6,7,8};
+      int n = 8;
+      MyClass obj = new MyClass();
+      System.out.println(obj.findMissingElement(nums, n));
+    }
+    
+    private int findMissingElement(int[] nums, int n) {
+        int low = 0;
+        int high = n - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low)/2;
+            
+            if (nums[mid] == mid + 1) {
+                low = mid + 1;
+            }else {
+                high = mid;
+            }
+        }
+        
+        return low + 1;
+    }
+}
